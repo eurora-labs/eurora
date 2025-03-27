7d80468046621a976e8c92672bc0ea8530b98a76
Andre Roelofs <andre@eurora-labs.com>
2025-03-27 08:28:13 -0700

    feat(@eurora/web): add a workaround to display skeleton before the form loads

--- apps/web/src/routes/(marketing)/waitlist_form.svelte
+++ apps/web/src/routes/(marketing)/waitlist_form.svelte
@@ -1,23 +1,20 @@
 <script lang="ts">
     import { onMount, onDestroy } from 'svelte';
-    import { Skeleton } from '@eurora/ui';
   
     const isDev = import.meta.env.DEV;
-
+  
     let {
         portalId,
         formId,
         submitText = 'Submit',
         region = 'na1'
     } = $props();
-  
     // export let portalId: string,
     //   formId: string | number,
     //   submitText = 'Submit',
     //   region = 'na1';
   
     const targetElementID = `form-${formId}`;
-    let formLoaded = $state(false);
   
     onMount(() => {
       if (isDev) console.log(`component:HubspotFormSvelte ${formId} mounted`);
@@ -34,10 +31,6 @@
           submitText,
           target: '#' + targetElementID,
           css: '', //undocumented but required for iframe styling
-          onFormReady: function (form: any) {
-            formLoaded = true;
-            if (isDev) console.log(`component:HubspotFormSvelte ${formId} form loaded`);
-          }
         });
       }
     };
@@ -46,26 +39,14 @@
       if (isDev) console.log(`component:HubspotFormSvelte ${formId} destroyed`);
     });
   </script>
-
-
+  
+  
   <svelte:head>
     <script
-        charset="utf-8"
-        type="text/javascript"
-        src="//js.hsforms.net/forms/embed/v2.js"
-        onload={handleCreateForm}></script>
+      src="//js.hsforms.net/forms/embed/v2.js"
+      onload={handleCreateForm}></script>
   </svelte:head>
-
-  {#if !formLoaded}
-    <div class="w-full space-y-2">
-      <Skeleton class="h-10 w-full" />
-      <Skeleton class="h-10 w-full" />
-      <Skeleton class="h-10 w-full" />
-      <Skeleton class="h-10 w-3/4 mx-auto" />
-    </div>
-  {/if}
-
+  
   <div
     id={targetElementID}
-    class="base-hubspot-form"
-    ></div>
+    class="base-hubspot-form" ></div>
