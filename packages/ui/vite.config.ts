33dc8813297aa7acf781780ecec2c84148180102
Andre Roelofs <andreroelofsai@gmail.com>
2025-03-29 12:06:02 -0700

    fix(@crates/eur-tauri): incorrect port definition

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
+import path from 'path';
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@eurora/katex': path.resolve(__dirname, '../custom-components/katex/src/lib/index.ts')
+		}
+	}
 });
