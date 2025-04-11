a6a392595eb77ae8ba864538211b0fa2337a4d12
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-04-12 00:53:42 +0200

    style(prettier): format files

--- apps/web/src/routes/vcards/linda_rosner/+server.ts
+++ apps/web/src/routes/vcards/linda_rosner/+server.ts
@@ -3,11 +3,11 @@ export const prerender = true;
  * @type {import('@sveltejs/kit').RequestHandler}
  */
 export async function GET() {
-    // Redirect to the .vcf endpoint
-    return new Response(null, {
-        status: 301,
-        headers: {
-            'Location': 'https://eurora-labs.com'
-        }
-    });
+	// Redirect to the .vcf endpoint
+	return new Response(null, {
+		status: 301,
+		headers: {
+			Location: 'https://eurora-labs.com'
+		}
+	});
 }
