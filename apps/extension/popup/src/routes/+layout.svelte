f1a8332aed3a62b224140c4174cdaef4951aff3b
Andre Roelofs <andreroelofsai@gmail.com>
2025-04-13 17:32:15 +0200

    feat(@eurora/extension-popup): create a placeholder popup

---
+++ apps/extension/popup/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.pcss';
+	// import Terminal from "@lucide/svelte/icons/terminal";
+	let { children } = $props();
+</script>
+
+<div class="min-h-screen container mx-auto"> 
+
+	{@render children?.()}
+</div>
