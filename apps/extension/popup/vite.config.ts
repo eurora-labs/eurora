a298b9d910f1c301d8eb4e8cdef8f97a20f66b6f
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-04-13 17:32:15 +0200

    feat(@eurora/extension-popup): create a placeholder popup

---
+++ apps/extension/popup/vite.config.ts
@@ -0,0 +1,25 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [sveltekit()],
+    build: {
+        outDir: path.join(__dirname, '../../../extensions/chromium/pages/popup'),
+        emptyOutDir: true,
+        reportCompressedSize: true,
+        commonjsOptions: {
+            transformMixedEsModules: true
+        },
+        cssCodeSplit: true,
+    },
+	resolve: {
+		alias: {
+			'@eurora/ui': path.resolve(__dirname, '../../../packages/ui/src'),
+			'@eurora/katex': path.resolve(
+				__dirname,
+				'../../packages/custom-components/katex/src/lib/index.ts'
+			)
+		}
+	}
+});
