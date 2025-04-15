82264ee5298d9d78e9db6167e05369cfffdba48e
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-04-15 04:57:05 +0200

    fix(@eurora/launcher): url mismatches during production builds

--- crates/app/eur-tauri/src/window.rs
+++ crates/app/eur-tauri/src/window.rs
@@ -67,7 +67,6 @@ pub fn create(
     label: &str,
     window_relative_url: String,
 ) -> tauri::Result<tauri::WebviewWindow> {
-    tracing::info!("creating window '{label}' created at '{window_relative_url}'");
     let window = tauri::WebviewWindowBuilder::new(
         handle,
         label,
@@ -88,10 +87,15 @@ pub fn create_launcher(
     label: &str,
     window_relative_url: String,
 ) -> tauri::Result<tauri::WebviewWindow> {
+    eprintln!("creating window '{label}' created at '{window_relative_url}'");
+
     let window = tauri::WebviewWindowBuilder::new(
         handle,
         label,
-        tauri::WebviewUrl::External(Url::parse("http://localhost:1420/launcher").unwrap()),
+        tauri::WebviewUrl::App(window_relative_url.into()),
+
+        // tauri::WebviewUrl::External(Url::parse("http://localhost:1420/launcher").unwrap()),
+        // tauri::WebviewUrl::External(Url::parse(&format!("{}/launcher", window_relative_url)).unwrap()),
     )
     .resizable(false)
     .inner_size(575.0, 50.0)
@@ -136,7 +140,7 @@ pub fn create_launcher(
     let window = tauri::WebviewWindowBuilder::new(
         handle,
         label,
-        tauri::WebviewUrl::External(Url::parse("http://localhost:1420/launcher").unwrap()),
+        tauri::WebviewUrl::External(Url::parse(&format!("{}/launcher", window_relative_url)).unwrap()),
     )
     .resizable(false)
     .inner_size(575.0, 50.0)
