c5df795cec37b655ad6c92966cf3cb2f1b642c35
Andre Roelofs <andreroelofsai@gmail.com>
2025-04-16 09:28:48 +0200

    feat(@eurora/docs): add tailwind and postcss configs

--- apps/docs/src/routes/+layout.svelte
+++ apps/docs/src/routes/+layout.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
 	import '../app.pcss';
-	// import { MainSidebar } from '@eurora/main-sidebar';
+	import { MainSidebar } from '@eurora/main-sidebar';
+	import { Sidebar, Breadcrumb } from '@eurora/ui';
 
 	let { children } = $props();
 </script>
 
+<Sidebar.Provider class="min-h-min" open={false}>
+	<MainSidebar />
+	<Sidebar.Inset class="min-h-min">
+		<header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+			<Sidebar.Trigger class="-ml-1" />
+			<Breadcrumb.Root>
+				<Breadcrumb.List>
+					<Breadcrumb.Item class="hidden md:block">
+						<Breadcrumb.Link>Eurora</Breadcrumb.Link>
+					</Breadcrumb.Item>
+					<Breadcrumb.Separator class="hidden md:block" />
+					<Breadcrumb.Item>
+						<Breadcrumb.Page>Overlay</Breadcrumb.Page>
+					</Breadcrumb.Item>
+					<Breadcrumb.Separator class="hidden md:block" />
+				</Breadcrumb.List>
+			</Breadcrumb.Root>
+		</header>
+		<main class="flex-1 overflow-auto p-4">
+			<div class="mx-auto h-full max-w-screen-xl">
+				{@render children?.()}
+			</div>
+		</main>
+	</Sidebar.Inset>
+</Sidebar.Provider>
+<!-- 
 <main class="flex-1 overflow-hidden bg-transparent">
 	{@render children?.()}
-</main>
+</main> -->
