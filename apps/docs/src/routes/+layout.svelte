6c7c4a78945e988226ca324857ad886ac06971be
Andre Roelofs <andreroelofsai@gmail.com>
2025-05-02 19:34:35 +0200

    feat(@eurora/docs): add launcher component and improve sidebar trigger

--- apps/docs/src/routes/+layout.svelte
+++ apps/docs/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.pcss';
-	import { Sidebar, Breadcrumb } from '@eurora/ui';
+	import { Sidebar } from '@eurora/ui';
 	import MainSidebar from '$lib/components/MainSidebar.svelte';
 
 	let { children } = $props();
@@ -9,29 +9,7 @@
 <Sidebar.Provider class="min-h-min" open={true}>
 	<MainSidebar />
 	<Sidebar.Inset class="min-h-min">
-		<header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-			<Sidebar.Trigger class="-ml-1" />
-			<Breadcrumb.Root>
-				<Breadcrumb.List>
-					<Breadcrumb.Item class="hidden md:block">
-						<Breadcrumb.Link>Eurora</Breadcrumb.Link>
-					</Breadcrumb.Item>
-					<Breadcrumb.Separator class="hidden md:block" />
-					<Breadcrumb.Item>
-						<Breadcrumb.Page>Test Doc</Breadcrumb.Page>
-					</Breadcrumb.Item>
-					<Breadcrumb.Separator class="hidden md:block" />
-				</Breadcrumb.List>
-			</Breadcrumb.Root>
-		</header>
-		<main class="flex-1 overflow-auto p-4">
-			<div class="mx-auto h-full max-w-screen-xl">
-				{@render children?.()}
-			</div>
-		</main>
+		<Sidebar.Trigger class="fixed bottom-0 left-0 m-[0.5rem]" />
+		{@render children?.()}
 	</Sidebar.Inset>
 </Sidebar.Provider>
-<!-- 
-<main class="flex-1 overflow-hidden bg-transparent">
-	{@render children?.()}
-</main> -->
