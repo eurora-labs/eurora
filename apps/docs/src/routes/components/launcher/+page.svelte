15c2fd97ef95615910b45b5f78ec94e37af0be0b
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-05-02 20:46:32 +0200

    feat(@eurora/docs): add initial simulation of the eurora launcher

--- apps/docs/src/routes/components/launcher/+page.svelte
+++ apps/docs/src/routes/components/launcher/+page.svelte
@@ -1,4 +1,34 @@
 <script lang="ts">
+	// import { Search } from '@lucide/svelte';
+	import { Input } from '@eurora/ui';
 </script>
 
-<img class="h-full min-w-full" src="/sample_background.jpg" alt="Sample Background" />
+<div>
+	<div
+		class="launcher absolute left-1/2 top-1/4 h-[150px] w-[1100px] -translate-x-1/2 -translate-y-1/4"
+	>
+		<!-- <Search /> -->
+		<Input type="text" placeholder="Eurora Search" class="h-full w-full border-none text-[32px]" />
+	</div>
+	<img class=" w-full" src="/sample_background.jpg" alt="Sample Background" />
+</div>
+
+<style>
+	.launcher {
+		background: rgba(255, 255, 255, 0.2);
+		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+		backdrop-filter: blur(18px);
+		-webkit-backdrop-filter: blur(18px);
+	}
+
+	:global(input) {
+		font-size: 50px !important;
+		opacity: 0.5 !important;
+		color: black !important;
+	}
+	:global(input::placeholder) {
+		font-size: 50px !important;
+		opacity: 0.3 !important;
+		color: black !important;
+	}
+</style>
