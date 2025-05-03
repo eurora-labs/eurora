f43ec94d06ec6946bd018f96f89f6d6f8c6cb3fb
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-05-03 11:59:01 +0200

    fix(@eurora/launcher): remove border from launcher box

--- packages/custom-components/launcher/src/components/command-input.svelte
+++ packages/custom-components/launcher/src/components/command-input.svelte
@@ -12,14 +12,14 @@
 	}: CommandPrimitive.InputProps = $props();
 </script>
 
-<div class="flex items-center border-b px-3" data-command-input-wrapper="">
-	<Search class="mr-2 shrink-0 opacity-50" size="35" style="color: rgba(0, 0, 0, 0.8);" />
+<div class=" flex items-center border-none px-3" data-command-input-wrapper="">
+	<Search class="mr-2 shrink-0 opacity-50" size="40" style="color: rgba(0, 0, 0, 0.8);" />
 	<CommandPrimitive.Input
 		class={cn(
-			'placeholder:text-muted-foreground custom-input flex w-full rounded-md border-none bg-transparent py-3 text-[50px] text-base shadow-none outline-none focus:border-transparent focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+			'placeholder:text-muted-foreground custom-input flex w-full rounded-md border-none bg-transparent py-3 text-base shadow-none outline-none focus:border-transparent focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
 			className
 		)}
-		style="height: {height}; box-shadow: none; font-size: 50px; color: rgba(0, 0, 0, 0.5); padding-left: 0px;"
+		style="height: {height}; box-shadow: none; font-size: 40px; color: rgba(0, 0, 0, 0.5); padding-left: 0px;"
 		bind:ref
 		bind:value
 		{...restProps}
@@ -29,6 +29,5 @@
 <style>
 	:global(.custom-input::placeholder) {
 		color: rgba(0, 0, 0, 0.25);
-		/* color: black; */
 	}
 </style>
