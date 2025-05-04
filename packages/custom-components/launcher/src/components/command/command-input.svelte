311498782a6dc7e6cc845661f3ebd6e70dfeeaa9
Andre Roelofs <andreroelofsai@gmail.com>
2025-05-04 15:06:09 +0200

    feat(@eurora/launcher): add proper auto-resizable text area for the text prompt

--- packages/custom-components/launcher/src/components/command/command-input.svelte
+++ packages/custom-components/launcher/src/components/command/command-input.svelte
@@ -11,29 +11,31 @@
 		height = $bindable('100px'),
 		...restProps
 	}: CommandPrimitive.InputProps = $props();
+
+	ref?.addEventListener('input', () => {
+		if (!ref) return;
+		ref.style.height = `${ref.scrollHeight}px`; // Set to scroll height
+
+		console.log('ref', ref.scrollHeight);
+	});
+
+	function adjustHeight(event: Event) {}
 </script>
 
-<div class=" flex items-center border-none px-3" data-command-input-wrapper="">
-	<Search class="mr-2 shrink-0 opacity-50" size="40" style="color: rgba(0, 0, 0, 0.8);" />
+<div class="items-top flex h-fit border-none px-3" data-command-input-wrapper="">
+	<Search class="mr-2 mt-7 shrink-0 opacity-50" size="40" style="color: rgba(0, 0, 0, 0.8); " />
 	<CommandPrimitive.Input
 		class={cn(
-			' custom-input flex w-full rounded-md border-none bg-transparent py-3 text-base shadow-none outline-none focus:border-transparent focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+			'custom-input my-[15px] flex w-full rounded-md border-none bg-transparent py-0 shadow-none outline-none focus:border-transparent focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50',
 			className
 		)}
-		style="height: {height}; box-shadow: none; font-size: 40px; color: rgba(0, 0, 0, 0.5); padding-left: 0px; font-weight: 400;"
+		style="font-size: 40px; line-height: 70px; box-shadow: none;  color: rgba(0, 0, 0, 0.5); padding-left: 0px; font-weight: 400; height: 70px;"
 		bind:ref
 		bind:value
 		{...restProps}
 	>
 		{#snippet child({ props })}
-			<!-- <input class="text-black" bind:value {...props} /> -->
 			<InputArea bind:ref bind:value {...props} />
 		{/snippet}
 	</CommandPrimitive.Input>
 </div>
-
-<style>
-	/* :global(.custom-input::placeholder) {
-		color: rgba(0, 0, 0, 0.25);
-	} */
-</style>
