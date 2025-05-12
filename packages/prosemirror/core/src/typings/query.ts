ba5c8c1ee0a2b833c667f2087c294e2181a261fb
Andre Roelofs <andreroelofsai@gmail.com>
2025-05-12 17:35:24 +0200

    feat(@eurora/prosemirror-core): add position as optional property to SveltePMExtension

--- packages/prosemirror/core/src/typings/query.ts
+++ packages/prosemirror/core/src/typings/query.ts
@@ -1,11 +1,6 @@
 import { SveltePMExtension } from './extension.js';
 
-export interface QueryExtension {
-	position: number;
-	extension: SveltePMExtension;
-}
-
 export interface Query {
 	text: string;
-	extensions: QueryExtension[];
+	extensions: SveltePMExtension[];
 }
