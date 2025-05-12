9e07d2cc1c55df0bad715bd971ac8b27e087ed8b
Andre Roelofs <andreroelofsai@gmail.com>
2025-05-12 18:21:59 +0200

    fix(@eurora/launcher): incorrect input positioning for ProsemirrorEditor

--- packages/custom-components/launcher/src/components/input-area/input-area.svelte
+++ packages/custom-components/launcher/src/components/input-area/input-area.svelte
@@ -29,7 +29,7 @@
 
 <ProsemirrorEditor
 	bind:this={ref}
-	class={cn(className, 'h-[100px] text-[40px] leading-[40px]')}
+	class={cn(className, 'h-[70px] px-0 pb-2 text-[40px] leading-[40px]')}
 	{...restProps}
 />
 
