a05290bac3de15b89f2146218dbc9afe7275ef21
Andre Roelofs <andreroelofsai@gmail.com>
2025-05-12 18:17:59 +0200

    feat(@eurora/prosemirror-core): add dynamic loading of given classes

--- packages/custom-components/launcher/src/components/input-area/input-area.svelte
+++ packages/custom-components/launcher/src/components/input-area/input-area.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" module>
 	import { Editor as ProsemirrorEditor, type Query } from '@eurora/prosemirror-core';
 	import type { ClassValue } from 'svelte/elements';
+
 	export interface Props {
 		ref?: ProsemirrorEditor;
 		query: Query;
@@ -11,7 +12,6 @@
 
 <script lang="ts">
 	import { cn } from '@eurora/ui/utils.js';
-
 	import { onMount } from 'svelte';
 
 	let {
@@ -27,10 +27,22 @@
 	});
 </script>
 
-<ProsemirrorEditor bind:this={ref} />
+<ProsemirrorEditor
+	bind:this={ref}
+	class={cn(className, 'h-[100px] text-[40px] leading-[40px]')}
+	{...restProps}
+/>
 
-<!-- 
-<textarea
+<!-- <textarea
+	bind:value
+	class={cn(
+		'border-input focus-visible:ring-ring flex w-full resize-none overflow-hidden rounded-md border bg-transparent px-3 py-2 shadow-sm focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+		className
+	)}
+	{...restProps}
+></textarea> -->
+
+<!-- <textarea
 	bind:this={ref}
 	bind:value
 	class={cn(
@@ -40,7 +52,6 @@
 	oninput={adjustHeight}
 	{...restProps}
 ></textarea> -->
-
 <style>
 	textarea::placeholder {
 		color: rgba(0, 0, 0, 0.25);
