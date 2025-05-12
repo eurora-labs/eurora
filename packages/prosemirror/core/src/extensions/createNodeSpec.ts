b786274c7c103630c87cdda0d3dad507bf5d6014
Andre Roelofs <andreroelofsai@gmail.com>
2025-05-12 14:53:12 +0200

    feat(@eurora/prosemirror-core): add core package with utility and implementations

---
+++ packages/prosemirror/core/src/extensions/createNodeSpec.ts
@@ -0,0 +1,64 @@
+import { DOMOutputSpec, Node as PMNode, NodeSpec } from 'prosemirror-model';
+import { SveltePMNode } from '../typings/index.js';
+import { mount } from 'svelte';
+import { htmlToDOMOutputSpec } from './htmlToDOMOutputSpec.js';
+import { getAttrsWithOutputSpec } from './getAttrsWithOutputSpec.js';
+
+export async function createNodeSpec(pm_node: SveltePMNode<any>): Promise<NodeSpec> {
+	const { schema, component } = pm_node;
+	if (component && schema) {
+		const staticSpec = await createSpec(pm_node);
+		schema.toDOM = (node: PMNode) => {
+			const div = document.createElement('div');
+			const comp = mount(component, {
+				target: div,
+				props: {
+					node,
+					attrs: node.attrs,
+					contentDOM: () => undefined
+				}
+			}) as any;
+			if (!comp.ref) return staticSpec;
+			const spec = htmlToDOMOutputSpec(comp.ref);
+			return spec as unknown as DOMOutputSpec;
+		};
+		schema.parseDOM = [
+			...(schema.parseDOM || []),
+			{
+				tag: staticSpec[0],
+				getAttrs: (dom: HTMLElement | string) => {
+					if (dom instanceof HTMLElement) {
+						return getAttrsWithOutputSpec(staticSpec, dom, {
+							selector: []
+						});
+					}
+					return null;
+				}
+			}
+		];
+	} else if (!component && schema?.toDOM === undefined) {
+		throw Error(
+			`You must provide either Svelte component or schema.toDOM method for your Svelte PMNode!`
+		);
+	}
+	return schema;
+}
+
+export async function createSpec(node: SveltePMNode<any>): Promise<readonly [string, ...any[]]> {
+	const { attrs, component } = node;
+	if (!component) {
+		return [''];
+	}
+	const div = document.createElement('div');
+	// eslint-disable-next-line @typescript-eslint/await-thenable
+	const comp = (await mount(component, {
+		target: div,
+		props: {
+			node: undefined,
+			attrs,
+			contentDOM: () => undefined
+		} as any
+	})) as any;
+	const spec = htmlToDOMOutputSpec(comp.ref);
+	return spec as [string, ...any[]];
+}
---
+++ packages/prosemirror/core/src/typings/extension.ts
@@ -0,0 +1,67 @@
+import { NodeSpec, Node as PMNode, Schema } from 'prosemirror-model';
+import type { Component } from 'svelte';
+import { MarkViewConstructor, NodeViewConstructor } from 'prosemirror-view';
+import { Plugin } from 'prosemirror-state';
+import { default as Editor } from '../Editor.svelte';
+import { Cmd } from './pm.js';
+import type { MarkSpec } from 'prosemirror-model';
+
+export interface NodeProps<T> {
+	node: PMNode;
+	ref?: HTMLElement;
+	attrs: T;
+}
+
+export interface Initialized extends ExtensionData {
+	plugins: Plugin[];
+	schema: Schema;
+}
+
+export interface ExtensionData {
+	commands: { [name: string]: (...args: any[]) => Cmd };
+	marks: { [name: string]: MarkSpec };
+	markViews: { [name: string]: MarkViewConstructor };
+	nodes: { [name: string]: NodeSpec };
+	nodeViews: { [name: string]: NodeViewConstructor };
+	sortedKeymaps: { [key: string]: { cmd: Cmd; priority: number }[] };
+	svelteNodes: { [name: string]: SveltePMNode<any> };
+}
+export interface SveltePMMark {
+	schema?: MarkSpec;
+	markView?: MarkViewConstructor;
+}
+
+export interface SveltePMExtension {
+	name: string;
+	commands?: { [name: string]: (...args: any[]) => Cmd };
+	keymaps?: { [key: string]: Cmd | { cmd: Cmd; priority: number }[] };
+	store?: Record<string, any>;
+	marks?: {
+		[name: string]: SveltePMMark;
+	};
+	svelteNodes?: {
+		[name: string]: SveltePMNode<any>;
+	};
+	init?: (editor: Editor) => void;
+	plugins?: (editor: Editor, schema: Schema) => Plugin[];
+	destroy?: () => void;
+}
+
+export interface SveltePMNode<T> {
+	attrs?: T;
+	selectors?: string[];
+	schema: NodeSpec;
+	// component?: Component<NodeProps<T>>;
+	component?: Component<NodeProps<T>, {}, ''>;
+	nodeView?: (editor: Editor) => NodeViewConstructor;
+
+	// component?: Component;
+}
+
+// Props extends Record<string, any> = {},
+// 		Exports extends Record<string, any> = {},
+// 		Bindings extends keyof Props | '' = string
+
+export interface PMExtension {
+	nodes: { [name: string]: NodeSpec };
+}
