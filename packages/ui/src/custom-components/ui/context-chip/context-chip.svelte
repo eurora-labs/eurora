45d09d905a4a46b6eb0d9710d08a209f1330c6f4
Andre Roelofs <andreroelofsai@gmail.com>
2025-05-14 18:54:23 +0200

    feat(@eurora/ui): add context chip custom component

---
+++ packages/ui/src/custom-components/ui/context-chip/context-chip.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { type Variant, contextChipVariants } from './index.js';
+	import { cn } from '@eurora/ui/utils.js';
+
+	let className: string | undefined | null = undefined;
+	export let href: string | undefined = undefined;
+	export let variant: Variant = 'default';
+	export let onClick: ((event: MouseEvent) => void) | undefined = undefined;
+	export { className as class };
+</script>
+
+<svelte:element
+	this={href ? 'a' : 'span'}
+	{href}
+	role={onClick ? 'button' : undefined}
+	on:click={onClick}
+	class={cn(contextChipVariants({ variant, className }))}
+	{...$$restProps}
+>
+	<slot />
+</svelte:element>
