82ff02aa7c1d1aa39405e822f797b62f1a4dc5b7
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-17 15:44:25 +0200

    refactor(@eurora/ext-video): to use popover instead of dialog

--- packages/prosemirror/ext-video/src/Video.svelte
+++ packages/prosemirror/ext-video/src/Video.svelte
@@ -54,7 +54,7 @@
 </script>
 
 <script lang="ts">
-	import { Badge, ContextChip, Dialog, Label, Input, Button, buttonVariants } from '@eurora/ui';
+	import { Badge, ContextChip, Label, Input, Button, buttonVariants, Popover } from '@eurora/ui';
 	import type { SvelteNodeViewProps } from '@eurora/prosemirror-core';
 	export interface Props extends SvelteNodeViewProps<VideoAttrs> {
 		ref: HTMLElement;
@@ -79,30 +79,34 @@
 	}
 </script>
 
-<Dialog.Root>
-	<Dialog.Trigger>
+<Popover.Root>
+	<Popover.Trigger>
 		<ContextChip bind:ref data-hole {...attrs} onkeydown={handleKeyDown}>video</ContextChip>
-	</Dialog.Trigger>
-
-	<Dialog.Content class="sm:max-w-[425px]">
-		<Dialog.Header>
-			<Dialog.Title>Edit profile</Dialog.Title>
-			<Dialog.Description>
-				Make changes to your profile here. Click save when you're done.
-			</Dialog.Description>
-		</Dialog.Header>
-		<div class="grid gap-4 py-4">
-			<div class="grid grid-cols-4 items-center gap-4">
-				<Label for="name" class="text-right">Name</Label>
-				<Input id="name" value="Pedro Duarte" class="col-span-3" />
+	</Popover.Trigger>
+	<Popover.Content class="w-80">
+		<div class="grid gap-4">
+			<div class="space-y-2">
+				<h4 class="font-medium leading-none">Dimensions</h4>
+				<p class="text-muted-foreground text-sm">Set the dimensions for the layer.</p>
 			</div>
-			<div class="grid grid-cols-4 items-center gap-4">
-				<Label for="username" class="text-right">Username</Label>
-				<Input id="username" value="@peduarte" class="col-span-3" />
+			<div class="grid gap-2">
+				<div class="grid grid-cols-3 items-center gap-4">
+					<Label for="width">Width</Label>
+					<Input id="width" value="100%" class="col-span-2 h-8" />
+				</div>
+				<div class="grid grid-cols-3 items-center gap-4">
+					<Label for="maxWidth">Max. width</Label>
+					<Input id="maxWidth" value="300px" class="col-span-2 h-8" />
+				</div>
+				<div class="grid grid-cols-3 items-center gap-4">
+					<Label for="height">Height</Label>
+					<Input id="height" value="25px" class="col-span-2 h-8" />
+				</div>
+				<div class="grid grid-cols-3 items-center gap-4">
+					<Label for="maxHeight">Max. height</Label>
+					<Input id="maxHeight" value="none" class="col-span-2 h-8" />
+				</div>
 			</div>
 		</div>
-		<Dialog.Footer>
-			<Button type="submit">Save changes</Button>
-		</Dialog.Footer>
-	</Dialog.Content>
-</Dialog.Root>
+	</Popover.Content>
+</Popover.Root>
