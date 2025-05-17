455fc1409a74c2107d18c8550ea51e1cc54ced47
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-05-17 13:36:44 +0200

    feat(@eurora/desktop): move creation of the video context chip from editor to launcher

--- packages/prosemirror/core/src/Editor.svelte
+++ packages/prosemirror/core/src/Editor.svelte
@@ -74,30 +74,6 @@
 				markViews: created.markViews
 			}
 		);
-
-		experimentalAddTranscript();
-	}
-
-	function experimentalAddTranscript() {
-		cmd((state, dispatch) => {
-			const tr = state.tr;
-			const { schema } = state;
-			const nodes = schema.nodes;
-			// tr.insert(
-			// 	1,
-			// 	nodes.transcript.createChecked(
-			// 		{ id: 'transcript-1', text: 'Exercise Sheet 2' },
-			// 		schema.text(' ')
-			// 	)
-			// );
-			tr.insert(
-				1,
-				nodes.transcript.createChecked({ id: 'transcript-2', text: 'video' }, schema.text(' '))
-			);
-
-			tr.setSelection(TextSelection.create(tr.doc, 0));
-			dispatch?.(tr);
-		});
 	}
 
 	export async function updateExtensions(newQuery: Query) {
