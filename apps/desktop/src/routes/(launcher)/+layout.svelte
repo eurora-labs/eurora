9bc55e8d14ae8aa10da77e6888cfb52d26168d63
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-17 15:16:23 +0200

    refactor(@eurora/desktop): platform-specific layout to be in root layout definition

--- apps/desktop/src/routes/(launcher)/+layout.svelte
+++ apps/desktop/src/routes/(launcher)/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import '../../app.pcss';
-	import { platform } from '@tauri-apps/plugin-os';
 	let { children } = $props();
 
-	document.body.classList.add(`${platform()}-app`);
 </script>
 
 <main class="flex-1 overflow-hidden bg-transparent">
