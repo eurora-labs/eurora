ae21b93777883b1d24654643c8365a3f2b342582
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-17 15:16:23 +0200

    refactor(@eurora/desktop): platform-specific layout to be in root layout definition

---
+++ apps/desktop/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { platform } from '@tauri-apps/plugin-os';
+	let { children } = $props();
+
+	document.body.classList.add(`${platform()}-app`);
+</script>
+
+{@render children?.()}
