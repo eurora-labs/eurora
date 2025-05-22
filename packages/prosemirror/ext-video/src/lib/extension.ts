829d6adabbc9d6119501e47db7e6b6fa6b0920a5
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-22 14:19:53 +0200

    refactor(@eurora/prosemirror-ext): the existing extensions with the best practices in mind

--- packages/prosemirror/ext-video/src/lib/extension.ts
+++ packages/prosemirror/ext-video/src/lib/extension.ts
@@ -1,11 +1,14 @@
 import { default as Video, videoAttrs, videoSchema } from './Video.svelte';
 import type { Component } from 'svelte';
 
-import { SveltePMExtension } from '@eurora/prosemirror-core';
+import { type SveltePMExtension } from '@eurora/prosemirror-core';
 import { SvelteNodeView } from '@eurora/prosemirror-core';
 export const ID = '9370B14D-B61C-4CE2-BDE7-B18684E8731A';
 
 export function videoExtension(): SveltePMExtension {
+	console.log('videoAttrs', videoAttrs);
+	console.log('videoSchema', videoSchema);
+	console.log('video', Video);
 	return {
 		name: ID,
 		svelteNodes: {
