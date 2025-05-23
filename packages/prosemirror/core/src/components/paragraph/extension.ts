c455f1d05b1bd6a5f1a667f84cac9dfecb25b9c6
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-23 12:56:52 +0200

    feat(@crates/eur-tauri): add a complete pipe to ChatGPT within the new ContextChip framework

--- packages/prosemirror/core/src/components/paragraph/extension.ts
+++ packages/prosemirror/core/src/components/paragraph/extension.ts
@@ -1,5 +1,5 @@
 import { default as Paragraph, paragraphAttrs, paragraphSchema } from './Paragraph.svelte';
-import type { SveltePMExtension } from '@prosemirror-svelte/core';
+import type { SveltePMExtension } from '@eurora/prosemirror-core';
 
 export function paragraphExtension() {
 	return {
