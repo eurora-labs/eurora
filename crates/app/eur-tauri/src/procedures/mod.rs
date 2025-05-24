d23fb15cd2b81fb3571e2b590773338a8821e7fd
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-24 14:53:56 +0200

    feat(@crates/eur-tauri): add monitor procedures

--- crates/app/eur-tauri/src/procedures/mod.rs
+++ crates/app/eur-tauri/src/procedures/mod.rs
@@ -1,6 +1,7 @@
 pub mod context_chip_procedures;
 // pub mod conversation_procedures;
+pub mod monitor_procedures;
 pub mod query_procedures;
-// pub mod system_procedures;
+pub mod system_procedures;
 pub mod third_party_procedures;
 pub mod window_procedures;
