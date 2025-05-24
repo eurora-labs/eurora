f18b40472fb6c84107c544d40b042324a7e98873
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-24 13:46:36 +0200

    refactor(@crates/eur-tauri): move procedures to a separate folder

---
+++ crates/app/eur-tauri/src/procedures/mod.rs
@@ -0,0 +1,6 @@
+pub mod context_chip_procedures;
+// pub mod conversation_procedures;
+pub mod query_procedures;
+// pub mod system_procedures;
+pub mod third_party_procedures;
+pub mod window_procedures;
