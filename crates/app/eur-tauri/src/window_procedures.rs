ad0e9a63d2d8c4d8454c4760bdb209fa41b95b06
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-24 12:15:22 +0200

    feat(@crates/eur-tauri): add initial integration of taurpc

---
+++ crates/app/eur-tauri/src/window_procedures.rs
@@ -0,0 +1,48 @@
+use tauri::{Manager, Runtime};
+#[taurpc::procedures(path = "window")]
+pub trait WindowApi {
+    async fn get_scale_factor<R: Runtime>(
+        app_handle: tauri::AppHandle<R>,
+        height: f64,
+    ) -> Result<f64, String>;
+
+    async fn resize_launcher_window<R: Runtime>(
+        app_handle: tauri::AppHandle<R>,
+        height: u32,
+        scale_factor: f64,
+    ) -> Result<(), String>;
+}
+
+#[derive(Clone)]
+pub struct WindowApiImpl;
+
+#[taurpc::resolvers]
+impl WindowApi for WindowApiImpl {
+    async fn get_scale_factor<R: Runtime>(
+        self,
+        app_handle: tauri::AppHandle<R>,
+        height: f64,
+    ) -> Result<f64, String> {
+        let window = app_handle.get_window("launcher").unwrap();
+        let current_size = window.inner_size().unwrap();
+        let scale_factor = (current_size.height as f64) / (height);
+        Ok(scale_factor)
+    }
+
+    async fn resize_launcher_window<R: Runtime>(
+        self,
+        app_handle: tauri::AppHandle<R>,
+        height: u32,
+        scale_factor: f64,
+    ) -> Result<(), String> {
+        let window = app_handle.get_window("launcher").unwrap();
+        let current_size = window.outer_size().unwrap();
+        let new_height = height as f64 * scale_factor;
+        let _ = window.set_size(tauri::Size::Physical(tauri::PhysicalSize {
+            width: current_size.width,
+            // height: new_height as u32 + 72,
+            height: new_height as u32,
+        }));
+        Ok(())
+    }
+}
