764a65570bcad6e9afb3ec3dc9509b8c3c97d802
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-05-27 09:10:24 +0200

    feat(@crates/eur-monolith): add db initialization

--- crates/backend/eur-monolith/src/main.rs
+++ crates/backend/eur-monolith/src/main.rs
@@ -5,10 +5,10 @@ use eur_proto::{
     proto_auth_service::proto_auth_service_server::ProtoAuthServiceServer,
     proto_ocr_service::proto_ocr_service_server::ProtoOcrServiceServer,
 };
+use eur_remote_db::DatabaseManager;
 use tonic::transport::Server;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Load environment variables
@@ -19,6 +19,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_max_level(Level::INFO)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("Failed to set tracing subscriber");
+
+    let db_manager =
+        DatabaseManager::new("postgres://postgres:postgres@localhost:5432/eurora").await?;
+
     let addr = "[::1]:50051".parse().unwrap();
     let ocr_service = OcrService::default();
     let auth_service = AuthService::default();
