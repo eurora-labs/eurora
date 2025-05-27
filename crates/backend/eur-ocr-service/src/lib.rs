dbeba0da9dfb6add077052d46a4f187d42e31562
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-27 08:33:21 +0200

    style(@crates): run cargo fix

--- crates/backend/eur-ocr-service/src/lib.rs
+++ crates/backend/eur-ocr-service/src/lib.rs
@@ -1,19 +1,14 @@
 //! The Eurora monolith server that hosts the gRPC service for questions.
 
 use anyhow::{Result, anyhow};
-use dotenv::dotenv;
 use eur_ocr::{self, OcrStrategy};
-use eur_proto::proto_ocr_service::proto_ocr_service_server::{
-    ProtoOcrService, ProtoOcrServiceServer,
-};
+use eur_proto::proto_ocr_service::proto_ocr_service_server::ProtoOcrService;
 use eur_proto::proto_ocr_service::{TranscribeImageRequest, TranscribeImageResponse};
 use eur_proto::shared::ProtoImage;
 use futures;
 use futures::future;
-use std::env;
-use tonic::{Request, Response, Status, transport::Server};
-use tracing::{Level, info};
-use tracing_subscriber::FmtSubscriber;
+use tonic::{Request, Response, Status};
+use tracing::info;
 
 #[derive(Default, Debug)]
 pub struct OcrService {}
