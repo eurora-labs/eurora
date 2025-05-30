7d20763a6d9730de878918a9d89e0ee20745c517
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-30 19:09:25 +0200

    feat(@eurora/web): implement refresh tokens call and simplify overal implementation

--- apps/web/src/lib/services/auth-service.ts
+++ apps/web/src/lib/services/auth-service.ts
@@ -1,10 +1,10 @@
-import { create } from '@bufbuild/protobuf';
-import { createConnectTransport, createGrpcWebTransport } from '@connectrpc/connect-web';
+import { createGrpcWebTransport } from '@connectrpc/connect-web';
 import { createClient, type Client } from '@connectrpc/connect';
 import {
 	ProtoAuthService,
 	type LoginRequest,
 	type LoginResponse,
+	type RefreshTokenRequest,
 	type RegisterRequest
 } from '@eurora/proto/auth_service';
 
@@ -21,23 +21,17 @@ class AuthService {
 	}
 
 	public async login(data: LoginRequest): Promise<LoginResponse> {
-		try {
-			const response = await this.client.login(data);
-			return response;
-		} catch (error) {
-			throw error;
-		}
+		return await this.client.login(data);
 	}
 
 	public async register(data: RegisterRequest): Promise<LoginResponse> {
-		try {
-			const response = await this.client.register(data);
-			return response;
-		} catch (error) {
-			throw error;
-		}
+		return await this.client.register(data);
+	}
+
+	public async refreshToken(data: RefreshTokenRequest): Promise<LoginResponse> {
+		return await this.client.refreshToken(data);
 	}
 }
 
 export const authService = new AuthService();
-export type { LoginRequest, LoginResponse };
+export type { LoginRequest, LoginResponse, RegisterRequest, RefreshTokenRequest };
