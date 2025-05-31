d31e70f967c830df2171bbd7f8ea3211c23f540f
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-31 15:40:25 +0200

    refactor(@eurora/web): tailwind3 to 4

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 console.log(path.resolve(__dirname, '../../packages/ui/src'));
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
 	resolve: {
 		alias: {
 			'@eurora/katex': path.resolve(
