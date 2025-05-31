0f4991277f79ba46f25a86f312ff77f89ecc60ba
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-31 15:05:53 +0200

    refactor(@eurora/ui): absolute to relative imports and fix the imports in other apps

--- packages/ui/src/lib/components/form/form-button.svelte
+++ packages/ui/src/lib/components/form/form-button.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import * as Button from '@eurora/ui/components/ui/button/index.js';
+	import * as Button from '$lib/components/button/index.js';
 
 	let { ref = $bindable(null), ...restProps }: Button.Props = $props();
 </script>
