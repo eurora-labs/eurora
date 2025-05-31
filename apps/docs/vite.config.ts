ab9c5c82a2934aa8b478e04acee34e7ca11d696a
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-31 18:36:26 +0200

    refactor(@eurora/launcher): convert to a buildable library

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -3,13 +3,5 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			'@eurora/launcher': path.resolve(
-				__dirname,
-				'../../packages/custom-components/launcher/src'
-			)
-		}
-	}
+	plugins: [sveltekit()]
 });
