be763ad62c127ce22b2aff380a6276a8b512eef9
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-31 18:59:41 +0200

    fix(@eurora/launcher): issues with only compiling for docs or desktop

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,7 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), tailwindcss()],
+	test: {
+		include: ['src/**/*.(test|spec).?(m)[jt]s?(x)'],
+		environment: 'jsdom'
+	}
 });
