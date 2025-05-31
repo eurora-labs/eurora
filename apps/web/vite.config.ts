c6788494dd113e6f13a8c94e18dd5aa74e1f6722
Andre Roelofs <p.arroelofs@gmail.com>
2025-05-31 17:43:09 +0200

    feat(@eurora/docs): progress on docs migration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
 
 console.log(path.resolve(__dirname, '../../packages/ui/src'));
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [sveltekit()],
 	resolve: {
 		alias: {
 			'@eurora/katex': path.resolve(
