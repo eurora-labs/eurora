1a681d39374f2dfbd23cd2cff45fe5915869938f
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-05-31 15:40:25 +0200

    refactor(@eurora/web): tailwind3 to 4

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+	// import Terminal from "@lucide/svelte/icons/terminal";
+	let { children } = $props();
+</script>
+
+<div class="container mx-auto min-h-screen">
+	{@render children?.()}
+</div>
