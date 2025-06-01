a44c385ec79a991578e0abeefb9502efa7214772
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-01 12:21:16 +0200

    feat(@eurora/ui): add initial storybook config

---
+++ packages/ui/src/lib/stories/context-chip/AllContextChip.stories.svelte
@@ -0,0 +1,29 @@
+<script module lang="ts">
+	import { ContextChip } from '$lib/custom-components/context-chip/index.js';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Inputs / ContextChip',
+		args: {
+			label: 'ContextChip'
+		}
+	});
+	const defaultProps = {
+		label: 'ContextChip',
+		reversedDirection: false
+	};
+</script>
+
+{#snippet contextChipGroup({
+	variant,
+	label,
+	style,
+	reversedDirection
+}: {
+	variant?: 'default' | 'primary' | 'secondary' | 'destructive' | 'outline';
+	label: string;
+	style?: 'default';
+	reversedDirection?: boolean;
+})}
+	<div class="group"></div>
+{/snippet}
