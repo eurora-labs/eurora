a2c70beff014aa1b2c2976f069b64809a11d9f76
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-02 19:00:41 +0200

    fix(@eurora/ui): various small issues with style definition

--- packages/ui/src/lib/custom-components/launcher/input-area.svelte
+++ packages/ui/src/lib/custom-components/launcher/input-area.svelte
@@ -38,27 +38,7 @@
 	bind:value
 	class={cn(
 		className,
-		'ProsemirrorEditor h-fit min-h-[100px] text-[40px] text-black/80 shadow-none'
+		'ProsemirrorEditor h-fit min-h-[100px] text-[40px] text-black/50 shadow-none'
 	)}
 	{...restProps}
 />
-
-<!-- <textarea
-	bind:value
-	class={cn(
-		'border-input focus-visible:ring-ring flex w-full resize-none overflow-hidden rounded-md border bg-transparent px-3 py-2 shadow-sm focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
-		className
-	)}
-	{...restProps}
-></textarea> -->
-
-<!-- <textarea
-	bind:this={ref}
-	bind:value
-	class={cn(
-		'border-input focus-visible:ring-ring flex w-full resize-none overflow-hidden rounded-md border bg-transparent px-3 py-2 shadow-sm focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
-		className
-	)}
-	oninput={adjustHeight}
-	{...restProps}
-></textarea> -->
