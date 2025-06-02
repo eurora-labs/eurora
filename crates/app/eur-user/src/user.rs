d8a9ad81559aab22e97bab0ba690a5e644f7a087
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-06-02 14:11:21 +0200

    feat(@crates/eur-tauri): properly implement the auth client and manager for user

---
+++ crates/app/eur-user/src/user.rs
@@ -0,0 +1,16 @@
+use chrono::prelude::*;
+use eur_secret::Sensitive;
+use serde::{Deserialize, Serialize};
+use std::cell::RefCell;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct User {
+    pub id: Uuid,
+    pub login: String,
+    pub email: Option<String>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    #[serde(skip_serializing)]
+    pub(super) access_token: RefCell<Option<Sensitive<String>>>,
+}
