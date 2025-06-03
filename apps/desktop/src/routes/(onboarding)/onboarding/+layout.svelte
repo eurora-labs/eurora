ee9563c7c723edf9fcfb1dc8d1c3d52e356bf80b
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-06-03 13:41:08 +0200

    feat(@eurora/desktop): add simple navigation for onboarding

--- apps/desktop/src/routes/(onboarding)/onboarding/+layout.svelte
+++ apps/desktop/src/routes/(onboarding)/onboarding/+layout.svelte
@@ -2,7 +2,7 @@
 	let { children } = $props();
 </script>
 
-<main class="flex h-screen">
-	<div class="w-2/3 p-8 overflow-y-auto">{@render children?.()}</div>
+<div class="flex h-screen w-screen">
+	<div class="w-2/3 p-8 h-full">{@render children?.()}</div>
 	<div class="w-1/3 h-full">static image</div>
-</main>
+</div>
