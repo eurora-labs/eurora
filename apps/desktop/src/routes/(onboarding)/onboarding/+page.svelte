44b12712c987e6de06e2d653f243c8f1d7b26692
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-03 13:12:30 +0200

    feat(@eurora/desktop): add skeleton option to set up llama

--- apps/desktop/src/routes/(onboarding)/onboarding/+page.svelte
+++ apps/desktop/src/routes/(onboarding)/onboarding/+page.svelte
@@ -2,4 +2,6 @@
 	import { Button } from '@eurora/ui/components/button/index';
 </script>
 
-<Button href="/onboarding/login">Button</Button>
+<Button href="/onboarding/login">Login / Sign Up</Button>
+
+<Button href="/onboarding/llama">Connect to Llama</Button>
