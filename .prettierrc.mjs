cdf6da905cde87fa5cbefaea9c844f477041bed1
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-04 09:40:42 +0200

    ci(deploy-monolith.yml): add line to login to AWS ECR

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -4,13 +4,14 @@
  */
 const config = {
 	useTabs: true,
+	tabWidth: 4,
 	singleQuote: true,
 	trailingComma: 'all',
 	printWidth: 100,
 	cssDeclarationSorterOrder: 'smacss',
 	plugins: ['prettier-plugin-svelte', 'prettier-plugin-css-order'],
 	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
-	endOfLine: 'auto'
+	endOfLine: 'auto',
 };
 
 export default config;
