779c2f582cff23ac799d90bc956bd8cdd80c2057
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-05 18:42:35 +0200

    feat(@eurora/web): add call to generate auth url for Google

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,16 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
 
-console.log(path.resolve(__dirname, '../../packages/ui/src'));
-
 export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
 			'@eurora/katex': path.resolve(
 				__dirname,
-				'../../packages/custom-components/katex/src/lib/index.ts'
-			)
-		}
-	}
+				'../../packages/custom-components/katex/src/lib/index.ts',
+			),
+		},
+	},
 });
