8f311cceb5ebd13487dd2c412f2d3ae2d919592a
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-07 21:39:27 +0200

    feat(@eurora/web): add slug reading during login for synced auth of desktop and web apps

---
+++ apps/web/src/routes/(auth)/login/[slug]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" module>
+	import type { PageProps } from '../$types';
+	interface Props extends PageProps {
+		data: {
+			slug: string;
+		};
+	}
+</script>
+
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	let { data }: Props = $props();
+	onMount(() => {
+		sessionStorage.setItem('loginToken', data.slug);
+		goto('/login');
+	});
+</script>
