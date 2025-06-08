cfd67a716c53d156f22aea64726ae6a9f9267ac7
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-08 15:28:37 +0200

    fix(@eurora/web): existence of pre-rendered slugs making build fail

---
+++ apps/web/svelte.config.js
@@ -0,0 +1,31 @@
+import adapterStatic from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess({ script: true }),
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapterStatic({
+			pages: 'dist',
+			assets: 'dist',
+			fallback: 'index.html',
+			precompress: true,
+			strict: true,
+		}),
+		paths: {
+			// Ensure assets are loaded correctly on GitHub Pages
+			assets: '',
+		},
+	},
+	compilerOptions: {
+		css: 'injected',
+		enableSourcemap: true,
+	},
+};
+
+export default config;
