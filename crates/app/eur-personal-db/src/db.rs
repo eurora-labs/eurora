c71fb70bb3d05b194d5934c7f92f1c93ed5fa791
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-09 18:02:44 +0200

    feat(@crates/eur-personal-db): add initial encryption settings

--- crates/app/eur-personal-db/src/db.rs
+++ crates/app/eur-personal-db/src/db.rs
@@ -4,10 +4,13 @@ use sqlite_vec::sqlite3_vec_init;
 use sqlx::Column;
 use sqlx::TypeInfo;
 use sqlx::migrate::MigrateDatabase;
+use sqlx::sqlite::SqliteConnectOptions;
 use sqlx::sqlite::{SqlitePool, SqlitePoolOptions};
 use sqlx::types::Uuid;
+use std::str::FromStr;
 use std::time::Duration;
 use tracing::debug;
+use tracing::info;
 
 use crate::types::{Activity, ActivityAsset, ChatMessage, Conversation};
 
@@ -22,32 +25,50 @@ impl DatabaseManager {
             database_path
         );
         let connection_string = format!("sqlite:{}", database_path);
+        eprintln!("Connection string: {}", connection_string);
 
         unsafe {
             sqlite3_auto_extension(Some(std::mem::transmute(sqlite3_vec_init as *const ())));
         }
 
-        // Create the database if it doesn't exist
-        if !sqlx::Sqlite::database_exists(&connection_string).await? {
-            sqlx::Sqlite::create_database(&connection_string).await?;
-        }
+        let opts = SqliteConnectOptions::from_str(&connection_string)?
+            .pragma("key", "placeholder")
+            .create_if_missing(true);
 
         let pool = SqlitePoolOptions::new()
             .max_connections(50)
-            .min_connections(3) // Minimum number of idle connections
+            .min_connections(3)
             .acquire_timeout(Duration::from_secs(10))
-            .connect(&connection_string)
+            .connect_with(opts)
             .await?;
 
+        sqlx::query("PRAGMA rekey = 'placeholder';")
+            .execute(&pool)
+            .await?;
         // Enable WAL mode
         sqlx::query("PRAGMA journal_mode = WAL;")
             .execute(&pool)
             .await?;
+        sqlx::query("PRAGMA cipher_page_size = 4096;")
+            .execute(&pool)
+            .await?;
+
+        sqlx::query("PRAGMA kdf_iter = 64000;")
+            .execute(&pool)
+            .await?;
+
+        sqlx::query("PRAGMA cipher_hmac_algorithm = 'HMAC-SHA512';")
+            .execute(&pool)
+            .await?;
+
+        sqlx::query("PRAGMA cipher_kdf_algorithm = 'PBKDF2-HMAC-SHA512';")
+            .execute(&pool)
+            .await?;
 
         // Enable SQLite's query result caching
-        // PRAGMA cache_size = -2000; -- Set cache size to 2MB
+        // PRAGMA cache_size = 2000; -- Set cache size to 2MB
         // PRAGMA temp_store = MEMORY; -- Store temporary tables and indices in memory
-        sqlx::query("PRAGMA cache_size = -2000;")
+        sqlx::query("PRAGMA cache_size = 2000;")
             .execute(&pool)
             .await?;
         sqlx::query("PRAGMA temp_store = MEMORY;")
