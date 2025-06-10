4b4144a1992a538fcb879caccf395731e05c714c
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-10 15:50:00 +0200

    chore(@chrome/pdf-watcher): add a todo to add a type

--- apps/extension/content-scripts/pdf-watcher/src/lib/types.ts
+++ apps/extension/content-scripts/pdf-watcher/src/lib/types.ts
@@ -3,6 +3,7 @@ import { MessageType, ChromeObj } from '@eurora/chrome-ext-shared/extensions/wat
 export type PdfMessageType = MessageType;
 
 export interface WatcherParams {
+	// TODO: Convert to PDFViewerApplication type from pdfjs lib
 	pdfViewerApplication?: any;
 }
 
