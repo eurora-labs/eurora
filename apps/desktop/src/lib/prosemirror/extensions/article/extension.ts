f7468cf73af95e75e43e56a072ba8e07d29ed2e9
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-13 15:25:35 +0200

    refactor(@eurora): change external package import from *.js to *

--- apps/desktop/src/lib/prosemirror/extensions/article/extension.ts
+++ apps/desktop/src/lib/prosemirror/extensions/article/extension.ts
@@ -1,5 +1,5 @@
 import { default as Article, articleAttrs, articleSchema } from './Article.svelte';
-import { Editor, type SveltePMExtension, SvelteNodeView } from '@eurora/prosemirror-core/index.js';
+import { Editor, type SveltePMExtension, SvelteNodeView } from '@eurora/prosemirror-core/index';
 import type { Component } from 'svelte';
 
 export const ID = '309f0906-d48c-4439-9751-7bcf915cdfc5';
