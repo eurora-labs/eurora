8c9ee760885cfd1471113e307bcbc69d5826c132
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-06-14 15:54:49 +0100

    feat(@crates/eur-timeline): empty out the existing focus tracker for windows

--- crates/proto/eur-proto/build.rs
+++ crates/proto/eur-proto/build.rs
@@ -9,13 +9,28 @@ fn main() -> Result<()> {
         .map(|entry| entry.path())
         .collect::<Vec<_>>();
 
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .protoc_arg("--experimental_allow_proto3_optional")
-        .out_dir("src/gen") // Output the generated files in a specific directory
-        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .compile_protos(&proto_files, &[proto_dir])?;
+    #[cfg(target_os = "windows")]
+    {
+        let common_dir = PathBuf::from("C:\\protoc\\include");
+        tonic_build::configure()
+            .build_server(true)
+            .build_client(true)
+            .compile_well_known_types(true)
+            .protoc_arg("--experimental_allow_proto3_optional")
+            .out_dir("src/gen") // Output the generated files in a specific directory
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+            .compile_protos(&proto_files, &[proto_dir, common_dir])?;
+    }
 
+    #[cfg(not(target_os = "windows"))]
+    {
+        tonic_build::configure()
+            .build_server(true)
+            .build_client(true)
+            .protoc_arg("--experimental_allow_proto3_optional")
+            .out_dir("src/gen") // Output the generated files in a specific directory
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+            .compile_protos(&proto_files, &[proto_dir])?;
+    }
     Ok(())
 }
