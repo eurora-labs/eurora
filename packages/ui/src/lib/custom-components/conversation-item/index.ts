9a26e4e75a9fa1e29660b9976a5aa0d56fc35685
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-17 11:21:08 +0200

    feat(@eurora/ui): migrate the conversation item to standard shadcn API

---
+++ packages/ui/src/lib/custom-components/conversation-item/index.ts
@@ -0,0 +1,7 @@
+import ConversationItemComponent, { type ConversationItemProps } from './conversation-item.svelte';
+
+export {
+	ConversationItemComponent,
+	ConversationItemComponent as ConversationItem,
+	type ConversationItemProps,
+};
