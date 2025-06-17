19634c4a7a8fc3db57486f66053afb4211fe8e64
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-17 12:54:43 +0200

    feat(@eurora/ui): add footer and sources options to message

--- packages/ui/src/lib/custom-components/conversation-item/index.ts
+++
@@ -1,7 +0,0 @@
-import ConversationItemComponent, { type ConversationItemProps } from './conversation-item.svelte';
-
-export {
-	ConversationItemComponent,
-	ConversationItemComponent as ConversationItem,
-	type ConversationItemProps,
-};
