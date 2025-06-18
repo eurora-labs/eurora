cb34c9a1449f9ede3f5fa734659be6463f747e36
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-06-18 17:29:32 +0200

    feat(@crates/eur-native-messaging): add handling for article snapshots

--- crates/app/eur-native-messaging/src/snapshot_context.rs
+++ crates/app/eur-native-messaging/src/snapshot_context.rs
@@ -1,9 +1,37 @@
 use anyhow::{Context, Result};
 use base64::prelude::*;
-pub use eur_proto::ipc::ProtoYoutubeSnapshot;
+pub use eur_proto::ipc::{ProtoArticleSnapshot, ProtoYoutubeSnapshot};
 pub use eur_proto::native_messaging::ProtoNativeYoutubeSnapshot;
 pub use eur_proto::shared::ProtoImage;
 use tracing::info;
+
+pub struct ArticleSnapshot(pub ProtoArticleSnapshot);
+
+pub struct NativeArticleSnapshot(pub ArticleSnapshot);
+
+impl From<&serde_json::Map<String, serde_json::Value>> for NativeArticleSnapshot {
+    fn from(obj: &serde_json::Map<String, serde_json::Value>) -> Self {
+        info!("NativeArticleSnapshot::from obj: {:?}", obj);
+        NativeArticleSnapshot(ArticleSnapshot(ProtoArticleSnapshot {
+            highlighted_content: obj
+                .get("highlightedText")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string(),
+        }))
+    }
+}
+
+impl TryFrom<&NativeArticleSnapshot> for ArticleSnapshot {
+    type Error = anyhow::Error;
+
+    fn try_from(obj: &NativeArticleSnapshot) -> Result<Self> {
+        Ok(ArticleSnapshot(ProtoArticleSnapshot {
+            highlighted_content: obj.0 .0.highlighted_content.clone(),
+        }))
+    }
+}
 
 // Wrapper type for ProtoYoutubeSnapshot
 pub struct YoutubeSnapshot(pub ProtoYoutubeSnapshot);
