ff2cf50955b767736c51dcf4dbc83bc76d1345f1
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-18 17:52:58 +0200

    refactor(@eurora/ui): chat to accept children similar to other shadcn components

--- packages/ui/src/lib/custom-components/chat/chat.svelte
+++ packages/ui/src/lib/custom-components/chat/chat.svelte
@@ -1,39 +1,19 @@
 <script lang="ts" module>
 	export interface ChatProps {
-		messages?: any[];
-		// messages?: MessageType[];
 		class?: string;
+		children?: any;
 	}
 </script>
 
 <script lang="ts">
 	import { ScrollArea } from '$lib/components/scroll-area/index.js';
-	import type MessageType from './message.js';
-	import * as Message from '$lib/custom-components/message/index.js';
-
-	let { messages = $bindable<MessageType[]>([]), class: className }: ChatProps = $props();
+	let { class: className, children }: ChatProps = $props();
 
 	let scrollAreaRef = $state<HTMLDivElement>();
 </script>
 
 <ScrollArea ref={scrollAreaRef} class="w-full {className}">
 	<div class="space-y-4 p-4 flex flex-col">
-		{#each messages as message}
-			<Message.Root
-				variant={message.role === 'user' ? 'default' : 'agent'}
-				finishRendering={() => {}}
-			>
-				<Message.Content>{message.content}</Message.Content>
-				{#if message.sources && message.sources.length > 0}
-					<Message.Footer>
-						<Message.Source>
-							{#each message.sources as source}
-								{@html source}
-							{/each}
-						</Message.Source>
-					</Message.Footer>
-				{/if}
-			</Message.Root>
-		{/each}
+		{@render children?.()}
 	</div>
 </ScrollArea>
