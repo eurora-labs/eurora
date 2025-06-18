4a97fefc3d8279d05f80193acc7f5482c5a4587b
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-18 19:39:22 +0200

    fix(@chrome/youtube-watcher): outdated youtube transcript retrieval api

---
+++ packages/ui/src/lib/custom-components/message/message-sources.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { cn, type WithElementRef } from '$lib/utils.js';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { Button, type ButtonProps } from '$lib/components/button/index.js';
+	let { ref = $bindable(null), class: className, children, ...restProps }: ButtonProps = $props();
+</script>
+
+<Button
+	{ref}
+	data-slot="message-sources"
+	class={cn('flex items-start', className)}
+	variant="ghost"
+	size="default"
+	{...restProps}
+>
+	{@render children?.()}
+	Sources
+</Button>
