09a51843c719452e502b91dbdd27b6cc8d3670d8
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-06-28 15:14:54 +0200

    feat(@eurora/desktop): add listeners to the endpoint connection service

--- crates/app/eur-tauri/src/procedures/mod.rs
+++ crates/app/eur-tauri/src/procedures/mod.rs
@@ -1,7 +1,7 @@
 pub mod auth_procedures;
 pub mod context_chip_procedures;
-// pub mod conversation_procedures;
 pub mod monitor_procedures;
+pub mod prompt_procedures;
 pub mod query_procedures;
 pub mod system_procedures;
 pub mod third_party_procedures;
