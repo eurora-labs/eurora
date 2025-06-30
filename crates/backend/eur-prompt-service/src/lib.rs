dcd31e460d6decc17ca6b4e2bcc19942bc6aef37
Andre Roelofs <p.arroelofs@gmail.com>
2025-06-30 14:38:58 +0200

    feat(@crates/eur-prompt-service): add support for llm inference on first party service

--- crates/backend/eur-prompt-service/src/lib.rs
+++ crates/backend/eur-prompt-service/src/lib.rs
@@ -1,17 +1,15 @@
-//! The Eurora Prompt service that provides gRPC endpoints for image transcription with JWT authentication.
-
 use anyhow::{Result, anyhow};
 use eur_auth::{Claims, JwtConfig, validate_access_token};
-use eur_prompt_kit::{LLMMessage, PromptKitService, Role};
+use eur_prompt_kit::{EurLLMService, LLMMessage, PromptKitService, RemoteConfig};
 
 use eur_proto::proto_prompt_service::{
     ProtoChatMessage, SendPromptRequest, SendPromptResponse,
     proto_prompt_service_server::ProtoPromptService,
 };
-use std::{error::Error, io::ErrorKind, net::ToSocketAddrs, pin::Pin, time::Duration};
+use std::pin::Pin;
 use tokio::sync::mpsc;
 use tokio_stream::{Stream, StreamExt, wrappers::ReceiverStream};
-use tonic::{Request, Response, Status, Streaming, transport::Server};
+use tonic::{Request, Response, Status};
 use tracing::{info, warn};
 
 /// Extract and validate JWT token from request metadata
@@ -46,8 +44,16 @@ pub struct PromptService {
 
 impl PromptService {
     pub fn new(jwt_config: Option<JwtConfig>) -> Self {
+        let mut prompt_service = PromptKitService::default();
+        prompt_service
+            .switch_to_remote(RemoteConfig {
+                provider: EurLLMService::OpenAI,
+                api_key: std::env::var("OPENAI_API_KEY").unwrap_or_default(),
+                model: "gpt-4o-2024-11-20".to_string(),
+            })
+            .unwrap();
         Self {
-            prompt_service: PromptKitService::default(),
+            prompt_service,
             jwt_config: jwt_config.unwrap_or_default(),
         }
     }
@@ -65,16 +71,17 @@ impl ProtoPromptService for PromptService {
         &self,
         request: Request<SendPromptRequest>,
     ) -> SendPromptResult<Self::SendPromptStream> {
+        info!("Received send_prompt request");
         let request_inner = request.into_inner();
+
         let messages = to_llm_message(request_inner.messages);
 
+        let (tx, rx) = mpsc::channel(128);
         let mut stream = self
             .prompt_service
             .chat_stream(messages)
             .await
             .map_err(|e| Status::internal(e.to_string()))?;
-
-        let (tx, rx) = mpsc::channel(128);
 
         tokio::spawn(async move {
             while let Some(item) = stream.next().await {
---
+++ crates/common/eur-proto-client/src/lib.rs
@@ -0,0 +1,16 @@
+mod clients;
+pub use clients::*;
+
+use anyhow::{Ok, Result, anyhow};
+use tonic::transport::{Channel, ClientTlsConfig};
+
+async fn get_secure_channel(base_url: String) -> Result<Option<Channel>> {
+    let tls = ClientTlsConfig::new().with_native_roots();
+    let channel = Channel::from_shared(base_url.clone())?
+        .tls_config(tls)?
+        .connect()
+        .await
+        .map_err(|e| anyhow!("Failed to connect to url: {}", e))?;
+
+    Ok(Some(channel))
+}
