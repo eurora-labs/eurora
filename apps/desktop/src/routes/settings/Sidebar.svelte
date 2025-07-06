05a1f4f86029afad1a4a717859f640f766810d89
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-06 15:34:42 +0200

    feat(@eurora/desktop): add skeleton for user settings

---
+++ apps/desktop/src/routes/settings/Sidebar.svelte
@@ -0,0 +1,82 @@
+<script lang="ts" module>
+	interface MenuItem {
+		title: string;
+		url: string;
+		icon: any;
+		isActive?: boolean;
+	}
+	let items: MenuItem[] = [
+		{
+			title: 'General',
+			url: '/settings',
+			icon: BoltIcon,
+		},
+		{
+			title: 'Hotkey',
+			url: '/settings/hotkey',
+			icon: KeyboardIcon,
+		},
+		{
+			title: 'Third party',
+			url: '/settings/third-party',
+			icon: WebhookIcon,
+		},
+		{
+			title: 'Telemetry',
+			url: '/settings/telemetry',
+			icon: ChevronsLeftRightEllipsis,
+		},
+	];
+</script>
+
+<script lang="ts">
+	import BoltIcon from '@lucide/svelte/icons/bolt';
+	import ChevronLeftIcon from '@lucide/svelte/icons/chevron-left';
+	import ChevronsLeftRightEllipsis from '@lucide/svelte/icons/chevrons-left-right-ellipsis';
+	import KeyboardIcon from '@lucide/svelte/icons/keyboard';
+	import WebhookIcon from '@lucide/svelte/icons/webhook';
+	import MailIcon from '@lucide/svelte/icons/mail';
+	import * as Sidebar from '@eurora/ui/components/sidebar/index';
+	import { Button } from '@eurora/ui/components/button/index';
+	import { page } from '$app/state';
+
+	let navigation = $derived(
+		items.map((item) => ({ ...item, isActive: item.url === page.url.pathname })),
+	);
+</script>
+
+<Sidebar.Root>
+	<Sidebar.Content>
+		<div class="flex items-center gap-2 pt-2">
+			<Button
+				variant="ghost"
+				class="rounded-none w-full flex justify-start text-[24px] font-normal"
+				href="/onboarding"
+			>
+				<ChevronLeftIcon class="size-6" />
+				Profile
+			</Button>
+		</div>
+		<Sidebar.Group>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each navigation as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton isActive={item.isActive}>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+	<Sidebar.Footer>
+		<Button>Contact us <MailIcon /></Button>
+	</Sidebar.Footer>
+</Sidebar.Root>
