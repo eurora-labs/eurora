2317d46147710b5d8a5b28b7777e0d2873ec2d3a
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-06 18:24:16 +0200

    refactor(@crates/eur-user): remove access and refresh tokens from user

--- crates/app/eur-user/src/user.rs
+++ crates/app/eur-user/src/user.rs
@@ -1,8 +1,5 @@
-use anyhow::{Context, Result};
 use chrono::prelude::*;
-use eur_secret::{Sensitive, secret};
 use serde::{Deserialize, Serialize};
-use std::cell::RefCell;
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Default)]
@@ -32,40 +29,4 @@ pub struct User {
     pub updated_at: DateTime<Utc>,
 
     pub hotkeys: Hotkeys,
-
-    #[serde(skip_serializing)]
-    pub(super) access_token: RefCell<Option<Sensitive<String>>>,
-
-    #[serde(skip_serializing)]
-    pub(super) refresh_token: RefCell<Option<Sensitive<String>>>,
-}
-
-impl User {
-    pub fn access_token(&self) -> Result<Sensitive<String>> {
-        if let Some(token) = self.access_token.borrow().as_ref() {
-            return Ok(token.clone());
-        }
-        let err_msg = "access token for user was deleted from keychain - login is now invalid";
-        let secret = secret::retrieve(
-            crate::auth::AuthManager::ACCESS_TOKEN_HANDLE,
-            secret::Namespace::BuildKind,
-        )?
-        .context(err_msg)?;
-        *self.access_token.borrow_mut() = Some(secret.clone());
-        Ok(secret)
-    }
-
-    pub fn refresh_token(&self) -> Result<Sensitive<String>> {
-        if let Some(token) = self.refresh_token.borrow().as_ref() {
-            return Ok(token.clone());
-        }
-        let err_msg = "refresh token for user was deleted from keychain - login is now invalid";
-        let secret = secret::retrieve(
-            crate::auth::AuthManager::REFRESH_TOKEN_HANDLE,
-            secret::Namespace::BuildKind,
-        )?
-        .context(err_msg)?;
-        *self.refresh_token.borrow_mut() = Some(secret.clone());
-        Ok(secret)
-    }
 }
