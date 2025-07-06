7509b462dbd3610f7005a9a02bca59e17072e2cd
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-06 15:52:46 +0200

    feat(@eurora/desktop): separate the hotkey change to a separate component

--- apps/desktop/src/routes/settings/hotkey/+page.svelte
+++ apps/desktop/src/routes/settings/hotkey/+page.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+	import Hotkey from '$lib/components/Hotkey.svelte';
 </script>
 
-Hotkey
+<div class="w-full h-full p-6 flex flex-col justify-between">
+	<h1 class="text-2xl font-bold">Set up Eurora hotkey</h1>
+
+	<div class="flex w-full h-full items-center justify-center">
+		<Hotkey />
+	</div>
+</div>
