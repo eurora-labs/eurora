7c2f82d4646c6dc825cc39f60269e89fd54d3a5c
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-06 15:34:42 +0200

    feat(@eurora/desktop): add skeleton for user settings

---
+++ apps/desktop/src/routes/settings/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as Sidebar from '@eurora/ui/components/sidebar/index';
+	import SettingsSidebar from './Sidebar.svelte';
+
+	let { children } = $props();
+</script>
+
+<Sidebar.Provider>
+	<SettingsSidebar />
+
+	<main>
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
