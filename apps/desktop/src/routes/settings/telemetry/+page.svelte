caac3bb784d199a370b2d8240dfef385ff86812c
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-06 19:20:20 +0200

    feat(@eurora/desktop): add skeleton telemetry switch

--- apps/desktop/src/routes/settings/telemetry/+page.svelte
+++ apps/desktop/src/routes/settings/telemetry/+page.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+	import { Switch } from '@eurora/ui/components/switch/index';
+	import { Label } from '@eurora/ui/components/label/index';
 </script>
 
-Telemetry
+<div class="w-full h-full p-6 flex flex-col justify-start items-start gap-2">
+	<h1 class="text-2xl font-bold">Telemetry</h1>
+	<p class="text-sm text-muted-foreground">
+		By enabling telemetry, you agree to share anonymous usage data with the Eurora team to help
+		improve the app.
+	</p>
+
+	<div class="flex w-full items-start justify-start gap-2 py-2">
+		<Switch />
+		<Label>Send anonymous usage data</Label>
+	</div>
+</div>
