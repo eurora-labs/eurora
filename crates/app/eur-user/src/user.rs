0d46bcf9445d46334ccafe35f4c4a6677b9f5764
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-06 12:28:39 +0200

    feat(@eurora/desktop): add skeleton code for hotkey setting

--- crates/app/eur-user/src/user.rs
+++ crates/app/eur-user/src/user.rs
@@ -5,12 +5,25 @@ use std::cell::RefCell;
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum HotkeyFunction {
+    OpenLauncher,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Hotkey {
+    pub key: String,
+    pub modifiers: Vec<String>,
+    pub function: HotkeyFunction,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct User {
     pub id: Uuid,
     pub login: String,
     pub email: Option<String>,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
+    pub hotkeys: Vec<Hotkey>,
 
     #[serde(skip_serializing)]
     pub(super) access_token: RefCell<Option<Sensitive<String>>>,
