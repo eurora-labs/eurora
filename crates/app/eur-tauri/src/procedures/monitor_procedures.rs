b1c5c644a44f80b84269d2ea6ef2abb0861edc0a
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-07-06 18:31:52 +0200

    refactor(@crates): remove deprecated eur-renderer and eur-screen-capture

--- crates/app/eur-tauri/src/procedures/monitor_procedures.rs
+++ crates/app/eur-tauri/src/procedures/monitor_procedures.rs
@@ -15,10 +15,7 @@ pub struct MonitorApiImpl;
 impl MonitorApi for MonitorApiImpl {
     async fn capture_monitor(self, monitor_id: String) -> Result<String, String> {
         let image = capture_monitor_by_id(monitor_id).unwrap();
-        let image = match cfg!(target_os = "linux") {
-            true => pollster::block_on(eur_renderer::blur_image(&image, 0.1, 36.0)),
-            false => image::DynamicImage::ImageRgba8(image).to_rgb8(),
-        };
+        let image = image::DynamicImage::ImageRgba8(image).to_rgb8();
 
         Ok(image_to_base64(image).unwrap())
     }
