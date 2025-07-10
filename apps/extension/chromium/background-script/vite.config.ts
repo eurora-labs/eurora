cac60d3abfeb35eaf18eebff32769293fdb6d9be
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-10 11:53:44 +0200

    build(@chrome/*): all extensions now compile to both firefox and chrome folders

--- apps/extension/chromium/background-script/vite.config.ts
+++ apps/extension/chromium/background-script/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
 		lib: {
 			// Could also be a dictionary or array of multiple entry points.
 			entry: 'src/index.ts',
-			name: 'youtube-watcher',
+			name: 'background-script',
 			fileName: 'index',
 			// Change this to the formats you want to support.
 			// Don't forget to update your package.json as well.
