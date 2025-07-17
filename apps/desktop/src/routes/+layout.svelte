2de22e551a396aeac25274acc668a19a66d54f06
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-07-17 12:56:55 +0200

    fix(@eurora): assign theme via mode watcher

--- apps/desktop/src/routes/+layout.svelte
+++ apps/desktop/src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
-	import '../app.pcss';
 	import '@eurora/ui/main.css';
+	import '../app.pcss';
+	import { ModeWatcher, setMode } from 'mode-watcher';
+	import { onMount } from 'svelte';
 	let { children } = $props();
+	onMount(() => {
+		setMode('light');
+	});
 </script>
+
+<ModeWatcher defaultMode="light" track={false} />
 
 <main class="h-screen p-0 m-0">
 	{@render children?.()}
