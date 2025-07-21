32ea14f163344a45d694fe694e48109d7c04055b
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-21 22:24:21 +0200

    feat(web): some improvement for contact us for user profile

--- apps/web/src/routes/settings/profile/+page.svelte
+++ apps/web/src/routes/settings/profile/+page.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
+	import { Button, buttonVariants } from '@eurora/ui/components/button/index';
+	import * as Dialog from '@eurora/ui/components/dialog/index';
 </script>
 
-<div>
-	<h1>Profile</h1>
-</div>
+<Dialog.Root>
+	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>Edit Profile</Dialog.Trigger>
+	<Dialog.Content class="sm:max-w-[425px]">
+		<Dialog.Header>
+			<Dialog.Title>Edit profile</Dialog.Title>
+			<Dialog.Description>
+				Make changes to your profile here. Click save when you're done.
+			</Dialog.Description>
+		</Dialog.Header>
+		hi content
+
+		<Dialog.Footer>
+			<Button type="submit">Save changes</Button>
+		</Dialog.Footer>
+	</Dialog.Content>
+</Dialog.Root>
