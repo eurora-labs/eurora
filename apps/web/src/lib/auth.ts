dfa094abb55b22229871b1a32ae239d2a7822614
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-21 20:15:53 +0200

    refactor(web): start moving to authjs

---
+++ apps/web/src/lib/auth.ts
@@ -0,0 +1,25 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import Apple from '@auth/sveltekit/providers/apple';
+import GitHub from '@auth/sveltekit/providers/github';
+import Google from '@auth/sveltekit/providers/google';
+import Credentials from '@auth/sveltekit/providers/credentials';
+
+export const { handle, signIn, signOut } = SvelteKitAuth({
+	// trustHost: true,
+	providers: [
+		Apple,
+		GitHub,
+		Google,
+		Credentials({
+			credentials: {
+				username: { label: 'Username' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(creds, request) {
+				const response = await fetch(request);
+				if (!response.ok) return null;
+				return (await response.json()) ?? null;
+			},
+		}),
+	],
+});
