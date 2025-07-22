247ba446b02fd5d34f7027fde35f3201a8e28c56
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-22 15:25:11 +0200

    fix(web): issues with building by checking the state of the environment

--- apps/web/src/routes/(auth)/signout/+page.server.ts
+++ apps/web/src/routes/(auth)/signout/+page.server.ts
@@ -1,4 +1,4 @@
-import { signOut } from '../../../auth.js';
+import { signOut } from '$lib/auth.js';
 import type { Actions } from './$types';
 
 export const actions = { default: signOut } satisfies Actions;
