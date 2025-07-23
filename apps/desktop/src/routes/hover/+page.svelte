4b85138ff76a3b95d5ff547cf1e71793f60348b5
Andre Roelofs <p.arroelofs@gmail.com>
2025-07-23 13:32:53 +0200

    feat(eur-tauri): add an initial hover window with placeholder github icon

---
+++ apps/desktop/src/routes/hover/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import SiGithub from '@icons-pack/svelte-simple-icons/icons/SiGithub';
+</script>
+
+<main class="container w-full h-full m-auto flex items-center justify-center">
+	<SiGithub size={24} />
+</main>
