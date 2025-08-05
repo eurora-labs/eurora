705d63633fc881dbf32347630b20e3ee83ad3e47
Andre Roelofs <andre@eurora-labs.com>
2025-08-05 14:20:01 +0200

    fix(eur-tauri): issues with proper screenshotting and positioning on windows

--- crates/app/eur-screen-position/src/windows/util.rs
+++ crates/app/eur-screen-position/src/windows/util.rs
@@ -1,5 +1,4 @@
 use crate::{CursorMonitorResult, MonitorInfo};
-use tracing::info;
 use xcap::Monitor;
 
 /// Find the monitor that contains the given cursor position
@@ -17,12 +16,10 @@ pub fn find_cursor_monitor(
             && cursor_position.y >= monitor_info.y as f64
             && cursor_position.y <= (monitor_info.y + monitor_info.height as i32) as f64
         {
-            info!("Cursor is on monitor: {}", monitor_info.id);
-            info!("Cursor X: {}", cursor_position.x);
-            info!("Cursor Y: {}", cursor_position.y);
             return Some(CursorMonitorResult {
                 monitor: monitor_info,
                 cursor_x: cursor_position.x,
+
                 cursor_y: cursor_position.y,
             });
         }
