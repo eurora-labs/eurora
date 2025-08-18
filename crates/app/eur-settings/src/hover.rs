ae007c609f8513b81526ab9aa86fac7839126838
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-08-18 12:28:19 +0200

    feat(eur-settings): add skeleton for app settings

---
+++ crates/app/eur-settings/src/hover.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct HoverSettings {
+    /// Whether hover window is enabled
+    pub enabled: bool,
+    /// Position of hover window
+    pub position: (i64, i64),
+}
