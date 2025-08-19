a6d19c18fdb83812b018d29c59c844a4b98b40e1
Andre Roelofs <p.arroelofs@gmail.com>
2025-08-19 11:20:57 +0200

    refactor(eur-settings): consolidate hover and telemetry to settings.rs and rename main struct

--- crates/app/eur-settings/src/settings.rs
+++ crates/app/eur-settings/src/settings.rs
@@ -13,3 +13,12 @@ pub struct TelemetrySettings {
     /// Distinct ID, if non-anonymous metrics are enabled
     pub distinct_id: Option<String>,
 }
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Type)]
+#[serde(rename_all = "camelCase")]
+pub struct HoverSettings {
+    /// Whether hover window is enabled
+    pub enabled: bool,
+    // /// Position of hover window
+    // pub position: (i64, i64),
+}
