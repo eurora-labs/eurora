6a6f5acdcdf29bea091bd80905642f86449209de
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-08-20 11:53:35 +0200

    refactor(eur-settings): improve some functions and fix minor issues

--- crates/app/eur-settings/src/settings.rs
+++ crates/app/eur-settings/src/settings.rs
@@ -31,8 +31,9 @@ pub struct GeneralSettings {
     pub autostart: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Type)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Type, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct LauncherSettings {
+    #[serde(default)]
     pub hotkey: Hotkey,
 }
