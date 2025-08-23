78003eca7e61a3a0e2a38035a54fb206f72c98a3
Andre Roelofs <andre@eurora-labs.com>
2025-08-23 14:50:27 +0200

    feat(desktop): add universal hotkey interpreter to tauri key

--- apps/desktop/src/lib/hotkey/hotkeyService.ts
+++ apps/desktop/src/lib/hotkey/hotkeyService.ts
@@ -1,22 +1,31 @@
 import { InjectionToken } from '@eurora/shared/context';
 import type { Hotkey } from '$lib/bindings/bindings.js';
-import { toString as event2String } from 'keyboard-event-to-string';
+import { toString as event2String, details as eventDetails } from 'keyboard-event-to-string';
 
 export const HotkeyServiceToken = new InjectionToken<HotkeyService>('HotkeyService');
 
 export class HotkeyService {
 	public interpretHotkey(event: KeyboardEvent): Hotkey | null {
+		const details = eventDetails(event);
+		if (!details.hasKey) return null;
 		const keys = event2String(event);
+
 		// The aggregated keys should contain at least one regular key
-		if (!keys.includes('Key') && !keys.includes('Space')) return null;
+		// if (
+		// 	!keys.includes('Key') &&
+		// 	!keys.includes('Digit') &&
+		// 	!keys.includes('Numpad') &&
+		// 	!keys.includes('Space')
+		// )
+		// 	return null;
+
+		console.log(keys);
 
 		const modifiers = keys.split(' + ');
 		let key = modifiers.pop();
-		if (key !== 'Space') {
-			key = key?.slice(3);
-		}
 
 		if (!key) throw new Error('Malformed hotkey string');
+		console.log(modifiers, key);
 
 		return { modifiers, key };
 	}
