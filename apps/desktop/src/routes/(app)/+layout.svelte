a70fe84340a58789d43cafcf0436ccda66c4a664
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-08-24 14:32:59 +0200

    feat(desktop): add main sidebar to desktop app

--- apps/desktop/src/routes/(app)/+layout.svelte
+++ apps/desktop/src/routes/(app)/+layout.svelte
@@ -2,6 +2,9 @@
 	import { platform } from '@tauri-apps/plugin-os';
 	import { onMount } from 'svelte';
 	import Menubar from '$lib/components/Menubar.svelte';
+	import MainSidebar from '$lib/components/MainSidebar.svelte';
+	import * as Sidebar from '@eurora/ui/components/sidebar/index';
+
 	let { children } = $props();
 	onMount(() => {
 		if (document) {
@@ -10,7 +13,10 @@
 	});
 </script>
 
-<div class="flex flex-col h-full">
-	<Menubar />
-	<div class="flex-1">{@render children?.()}</div>
-</div>
+<Menubar />
+<Sidebar.Provider>
+	<MainSidebar />
+	<main class="flex flex-col h-full w-full">
+		<div class="flex-1">{@render children?.()}</div>
+	</main>
+</Sidebar.Provider>
