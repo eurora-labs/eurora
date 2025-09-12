96fb7df85215f8bbb3d5f49e513cc0f5467b82a9
Andre Roelofs <p.arroelofs@gmail.com>
2025-09-12 15:36:44 +0200

    refactor(eur-native-messaging): the pipeline to minimize the usage of proto-based structs

---
+++ crates/app/eur-native-messaging/src/types/assets.rs
@@ -0,0 +1,48 @@
+use enum_dispatch::enum_dispatch;
+use serde::{Deserialize, Serialize};
+use specta::Type;
+
+#[allow(clippy::enum_variant_names)]
+#[enum_dispatch]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "kind", content = "data")]
+pub enum NativeAsset {
+    NativeYoutubeAsset,
+    NativeArticleAsset,
+    NativeTwitterAsset,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, Type)]
+pub struct NativeYoutubeAsset {
+    pub url: String,
+    pub title: String,
+    pub transcript: String,
+    pub current_time: f32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, Type)]
+pub struct NativeArticleAsset {
+    pub content: String,
+    pub text_content: String,
+    pub selected_text: Option<String>,
+    pub title: String,
+    pub site_name: String,
+    pub language: String,
+    pub excerpt: String,
+    pub length: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, Type)]
+pub struct NativeTwitterTweet {
+    pub text: String,
+    pub timestamp: Option<String>,
+    pub author: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, Type)]
+pub struct NativeTwitterAsset {
+    pub url: String,
+    pub title: String,
+    pub tweets: Vec<NativeTwitterTweet>,
+    pub timestamp: String,
+}
