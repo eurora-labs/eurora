6128a1b8dbf400b083d172a8d2bdffc67462a363
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-09-13 13:31:59 +0200

    refactor(eur-native-messaging): simplify the ipc comminication

--- crates/app/eur-native-messaging/src/types/mod.rs
+++ crates/app/eur-native-messaging/src/types/mod.rs
@@ -1,5 +1,23 @@
+use enum_dispatch::enum_dispatch;
+use serde::{Deserialize, Serialize};
+use strum_macros::AsRefStr;
+
 mod assets;
 mod snapshots;
 
 pub use assets::*;
 pub use snapshots::*;
+
+#[allow(clippy::enum_variant_names)]
+#[enum_dispatch]
+#[derive(Debug, Clone, Serialize, Deserialize, AsRefStr)]
+#[serde(tag = "kind", content = "data")]
+pub enum NativeMessage {
+    NativeYoutubeAsset,
+    NativeArticleAsset,
+    NativeTwitterAsset,
+
+    NativeYoutubeSnapshot,
+    NativeArticleSnapshot,
+    NativeTwitterSnapshot,
+}
