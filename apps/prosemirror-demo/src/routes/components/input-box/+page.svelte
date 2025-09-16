b01b8741ee37a70aa28ea6fd91eaecf3eb8c2098
Andre Roelofs <p.arroelofs@gmail.com>
2025-09-16 13:15:13 +0200

    feat(prosemirror-demo): add initial code to load article chip

--- apps/prosemirror-demo/src/routes/components/input-box/+page.svelte
+++ apps/prosemirror-demo/src/routes/components/input-box/+page.svelte
@@ -1,44 +1,81 @@
 <script lang="ts">
-	// import { LauncherNative as Launcher } from '@eurora/launcher';
+	import { Button } from '@eurora/ui/components/button/index';
 	import * as Launcher from '@eurora/prosemirror-view/launcher';
-	import { Editor as ProsemirrorEditor } from '@eurora/prosemirror-core/index';
+	import {
+		Editor as ProsemirrorEditor,
+		type SveltePMExtension,
+	} from '@eurora/prosemirror-core/index';
+	import { extensionFactory, registerCoreExtensions } from '@eurora/prosemirror-factory/index';
+
 	let editorRef: ProsemirrorEditor | undefined = $state(undefined);
 
-	let exampleInput = $state({
-		text: '',
-		extensions: [],
+	registerCoreExtensions();
+	let searchQuery = $state({
+		text: 't',
+		extensions: [
+			// extensionFactory.getExtension('9370B14D-B61C-4CE2-BDE7-B18684E8731A'),
+			extensionFactory.getExtension('7c7b59bb-d44d-431a-9f4d-64240172e092'),
+			extensionFactory.getExtension('309f0906-d48c-4439-9751-7bcf915cdfc5'),
+			extensionFactory.getExtension('2c434895-d32c-485f-8525-c4394863b83a'),
+		] as SveltePMExtension[],
 	});
 
-	function addExerciseSheet() {
+	function addArticleChip() {
 		editorRef?.cmd((state, dispatch) => {
 			const tr = state.tr;
 			const { schema } = state;
 			const nodes = schema.nodes;
 			const { $from: from } = state.selection;
+			console.log(nodes);
 			tr.insert(
 				from.pos,
-				nodes.transcript.createChecked(
-					{ id: 'transcript-1', text: 'Exercise Sheet 2' },
-					schema.text(' '),
+				nodes['309f0906-d48c-4439-9751-7bcf915cdfc5'].createChecked(
+					{ id: 'article-1', text: 'Article 1' },
+					schema.text('article'),
 				),
 			);
-
-			dispatch?.(tr);
 		});
 	}
+
+	// function addExerciseSheet() {
+	// 	editorRef?.cmd((state, dispatch) => {
+	// 		const tr = state.tr;
+	// 		const { schema } = state;
+	// 		const nodes = schema.nodes;
+	// 		const { $from: from } = state.selection;
+	// 		tr.insert(
+	// 			from.pos,
+	// 			nodes.transcript.createChecked(
+	// 				{ id: 'transcript-1', text: 'Exercise Sheet 2' },
+	// 				schema.text(' '),
+	// 			),
+	// 		);
+
+	// 		dispatch?.(tr);
+	// 	});
+	// }
 </script>
 
-<div>
-	<div class="launcher absolute top-1/4 left-1/2 w-[1100px] -translate-x-1/2">
-		<Launcher.Root class="rounded-lg border shadow-md">
-			<Launcher.Input placeholder="Search" bind:query={exampleInput} bind:editorRef />
-		</Launcher.Root>
+<div class="flex flex-col">
+	<div class="w-full h-1/2 justify-center items-center">
+		<div class="launcher absolute top-1/4 left-1/2 w-[1100px] -translate-x-1/2">
+			<Launcher.Root class="rounded-lg border shadow-md">
+				<Launcher.Input placeholder="Search" bind:query={searchQuery} bind:editorRef />
+			</Launcher.Root>
+		</div>
+		<img class=" w-full" src="/sample_background.jpg" alt="Sample Background" />
 	</div>
-	<img class=" w-full" src="/sample_background.jpg" alt="Sample Background" />
+
+	<div class="w-full h-1/2 bg-white flex flex-row">
+		<div class="flex flex-col gap-4">
+			<h2 class="text-2xl font-bold">Context Chips Controls</h2>
+			<Button onclick={addArticleChip}>Add article</Button>
+			<Button>Add youtube</Button>
+		</div>
+	</div>
 </div>
 
 <style lang="postcss">
-	/* @reference "@eurora/ui/main.css"; */
 	.launcher {
 		backdrop-filter: blur(36px);
 		-webkit-backdrop-filter: blur(36px);
