9916d8389bff1e215de6a13e82580778a66ab3fa
Andre Roelofs <p.arroelofs@gmail.com>
2025-09-25 15:38:22 +0200

    fix(eur-tauri): the app crashing if database can't be initialized

--- crates/app/eur-tauri/src/shared_types.rs
+++ crates/app/eur-tauri/src/shared_types.rs
@@ -3,7 +3,7 @@ use async_mutex::Mutex;
 use eur_personal_db::{Conversation, PersonalDatabaseManager};
 use eur_prompt_kit::PromptKitService;
 use eur_settings::AppSettings;
-use tracing::info;
+use tracing::error;
 
 use crate::util::get_db_path;
 pub type SharedPromptKitService = Mutex<Option<PromptKitService>>;
@@ -15,7 +15,7 @@ pub async fn create_shared_database_manager(
 ) -> Result<PersonalDatabaseManager> {
     let db_path = get_db_path(app_handle);
     PersonalDatabaseManager::new(&db_path).await.map_err(|e| {
-        info!("Failed to create database manager: {}", e);
+        error!("Failed to create database manager: {}", e);
         e.into()
     })
     // PersonalDatabaseManager::new(&db_path)
