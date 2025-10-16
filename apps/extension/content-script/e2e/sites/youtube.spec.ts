b8189a06fb30c6760a3446760d0369d95aabbc24
Andre Roelofs <p.arroelofs@gmail.com>
2025-10-16 14:12:57 +0200

    test(content-script): add snapshot testing for default and youtube

--- apps/extension/content-script/e2e/sites/youtube.spec.ts
+++ apps/extension/content-script/e2e/sites/youtube.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '../util/fixtures.ts';
 import { waitForBootstrap, waitForSiteMounted } from '../util/helpers.ts';
 
 test.describe('Youtube Watcher Tests', () => {
-	test('Should extract English subtitles from a video', async ({ page, sw }) => {
+	test('should extract English subtitles from a video', async ({ page, sw }) => {
 		await page.goto('https://www.youtube.com/watch?v=CXKoCMVqM9s&t=1289s');
 		await waitForBootstrap(page);
 		await waitForSiteMounted(page, 'youtube.com');
@@ -22,5 +22,24 @@ test.describe('Youtube Watcher Tests', () => {
 		expect(response.data).toBeDefined();
 		expect(Array.isArray(JSON.parse(response.data.transcript))).toEqual(true);
 		expect(response.data.current_time).toEqual(1289);
+	});
+
+	test('should extract video frame from a video', async ({ page, sw }) => {
+		await page.goto('https://www.youtube.com/watch?v=CXKoCMVqM9s&t=1289s');
+		await waitForBootstrap(page);
+		await waitForSiteMounted(page, 'youtube.com');
+
+		const response: WatcherResponse = await sw.evaluate(async () => {
+			const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+			return await new Promise((resolve) => {
+				chrome.tabs.sendMessage(tab.id!, { type: 'GENERATE_SNAPSHOT' }, (response) =>
+					resolve(response),
+				);
+			});
+		});
+		expect(response.kind).toEqual('NativeYoutubeSnapshot');
+		expect(response.data).toBeDefined();
+		expect(response.data.video_frame_base64).toBeDefined();
+		expect(response.data.video_frame_base64.length).toBeGreaterThan(0);
 	});
 });
