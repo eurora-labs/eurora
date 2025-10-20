4baf57be257690df7170f9a6f67a4c053415fd43
Andre Roelofs <p.arroelofs@gmail.com>
2025-10-20 16:33:27 +0200

    feat(ui): add initial structure for timeline item

---
+++ packages/ui/src/lib/custom-components/timeline/timeline-item.svelte
@@ -0,0 +1,9 @@
+<script lang="ts" module>
+	export interface TimelineItemProps {
+		iconBase64: string;
+	}
+</script>
+
+<script lang="ts">
+	let { iconBase64 }: TimelineItemProps = $props();
+</script>
