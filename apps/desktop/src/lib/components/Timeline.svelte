d305863cdaac9c3c4d099affe3392b3fd8ef6b83
Andre Roelofs <p.arroelofs@gmail.com>
2025-10-20 15:29:19 +0200

    feat(desktop): add initial timeline component

---
+++ apps/desktop/src/lib/components/Timeline.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-row w-1/2 bg-black text-white shadow-md h-[100px] px-4 items-center">
+	<div class="bg-amber-700 w-full h-[20px] text-center rounded-2xl">IC</div>
+	<div class="bg-blue-600 w-full h-[20px] text-center rounded-2xl">IC</div>
+	<div class="bg-red-600 w-full h-[20px] text-center rounded-2xl">IC</div>
+	<div class="bg-yellow-700 w-full h-[20px] text-center rounded-2xl">IC</div>
+	<div>Now</div>
+</div>
