213c0ba984c5c77737ab0aadc6e82ada9a25eb6a
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-10-21 14:23:25 +0200

    feat(ui): add proper code to the timeline item and setup simple storybook component

--- packages/ui/src/lib/custom-components/timeline/timeline-item.svelte
+++ packages/ui/src/lib/custom-components/timeline/timeline-item.svelte
@@ -1,9 +1,31 @@
 <script lang="ts" module>
 	export interface TimelineItemProps {
-		iconBase64: string;
+		color?: string;
+		children?: any;
 	}
 </script>
 
 <script lang="ts">
-	let { iconBase64 }: TimelineItemProps = $props();
+	let { color = 'white', children }: TimelineItemProps = $props();
+
+	let borderColor = $state('white');
+	$effect(() => {
+		if (color == 'white') {
+			borderColor = 'black';
+		} else {
+			borderColor = color;
+		}
+	});
 </script>
+
+<div class="relative w-full h-[50px] flex items-center justify-center rounded-2xl">
+	<div
+		class="absolute w-full h-[15px] rounded-2xl top-1/2 -translate-y-1/2 border-solid border-1"
+		style="background-color: {color}; border-color: {borderColor};"
+	></div>
+	<div
+		class="relative z-10 flex items-center justify-center w-fit h-full text-sm text-center p-0 m-0"
+	>
+		{@render children?.()}
+	</div>
+</div>
