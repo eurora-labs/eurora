939b6b7deca4b0d4c475c386db3f61d077a9b198
Andre Roelofs <p.arroelofs@gmail.com>
2025-10-21 14:23:25 +0200

    feat(ui): add proper code to the timeline item and setup simple storybook component

---
+++ packages/ui/src/lib/custom-components/timeline/timeline.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" module>
+	export interface TimelineProps {
+		class?: string;
+		children?: any;
+	}
+</script>
+
+<script lang="ts">
+	import { ScrollArea } from '$lib/components/scroll-area/index.js';
+	let { class: className, children }: TimelineProps = $props();
+
+	let scrollAreaRef = $state<HTMLDivElement>();
+</script>
+
+<ScrollArea ref={scrollAreaRef} orientation="horizontal" class="w-1/2 {className}">
+	<div class="flex flex-row gap-2">
+		{@render children?.()}
+	</div>
+</ScrollArea>
