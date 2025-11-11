40c9142e3dd5eb9a7046df57bfa57139bc7cc269
Andre Roelofs <p.arroelofs@gmail.com>
2025-11-11 17:45:18 +0100

    refactor(eur-timeline): to delegate the finer activity tracking to the strategies

--- crates/app/eur-activity/src/lib.rs
+++ crates/app/eur-activity/src/lib.rs
@@ -32,7 +32,7 @@ pub use snapshots::{ArticleSnapshot, DefaultSnapshot, TwitterSnapshot, YoutubeSn
 pub use storage::{ActivityStorage, ActivityStorageConfig, SaveableAsset, SavedAssetInfo};
 pub use strategies::ActivityStrategy;
 // Re-export strategy types
-pub use strategies::{BrowserStrategy, DefaultStrategy, NoStrategy};
+pub use strategies::{ActivityReport, BrowserStrategy, DefaultStrategy, NoStrategy};
 pub use types::{
     Activity, ActivityAsset, ActivitySnapshot, AssetFunctionality, ContextChip, DisplayAsset,
 };
