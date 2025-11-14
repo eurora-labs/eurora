f19b7f05c52af58f548b0f81d0eb77c428efdeb2
Andre Roelofs <p.arroelofs@gmail.com>
2025-11-14 15:29:40 +0100

    feat(eur-native-messaging): rewrite the server definition from double client server to duplex

---
+++ crates/app/eur-native-messaging/build.rs
@@ -0,0 +1,35 @@
+use std::{io::Result, path::PathBuf};
+
+fn main() -> Result<()> {
+    let proto_dir = PathBuf::from("../../../proto");
+    let proto_files = std::fs::read_dir(&proto_dir)?
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.path().extension().is_some_and(|ext| ext == "proto"))
+        .map(|entry| entry.path())
+        .collect::<Vec<_>>();
+
+    #[cfg(target_os = "windows")]
+    {
+        let common_dir = PathBuf::from("C:\\protoc\\include");
+        tonic_prost_build::configure()
+            .build_server(true)
+            .build_client(true)
+            // .compile_well_known_types(true)
+            .protoc_arg("--experimental_allow_proto3_optional")
+            // .out_dir("src/gen") // Output the generated files in a specific directory
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+            .compile_protos(&proto_files, &[proto_dir, common_dir])?;
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        tonic_prost_build::configure()
+            .build_server(true)
+            .build_client(true)
+            .protoc_arg("--experimental_allow_proto3_optional")
+            // .out_dir("src/gen") // Output the generated files in a specific directory
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+            .compile_protos(&proto_files, &[proto_dir])?;
+    }
+    Ok(())
+}
