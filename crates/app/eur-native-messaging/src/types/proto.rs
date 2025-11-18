c388b2b5772346e2f6af8c867efb776867406ee1
Andre Roelofs <p.arroelofs@gmail.com>
2025-11-18 15:26:57 +0100

    refactor(browser_bridge.proto): to the new simplified signature

---
+++ crates/app/eur-native-messaging/src/types/proto.rs
@@ -0,0 +1,48 @@
+mod proto {
+    tonic::include_proto!("browser_bridge");
+}
+
+pub use proto::*;
+
+// The code below is useful only to avoid super ugly frame type
+// to parent frame conversions
+
+impl From<RequestFrame> for Frame {
+    fn from(value: RequestFrame) -> Self {
+        Frame {
+            kind: Some(frame::Kind::Request(value)),
+        }
+    }
+}
+
+impl From<ResponseFrame> for Frame {
+    fn from(value: ResponseFrame) -> Self {
+        Frame {
+            kind: Some(frame::Kind::Response(value)),
+        }
+    }
+}
+
+impl From<EventFrame> for Frame {
+    fn from(value: EventFrame) -> Self {
+        Frame {
+            kind: Some(frame::Kind::Event(value)),
+        }
+    }
+}
+
+impl From<ErrorFrame> for Frame {
+    fn from(value: ErrorFrame) -> Self {
+        Frame {
+            kind: Some(frame::Kind::Error(value)),
+        }
+    }
+}
+
+impl From<CancelFrame> for Frame {
+    fn from(value: CancelFrame) -> Self {
+        Frame {
+            kind: Some(frame::Kind::Cancel(value)),
+        }
+    }
+}
