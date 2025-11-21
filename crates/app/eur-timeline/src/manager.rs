ed0d242a56d58e8d445076e64f1731f297bcb502
Andre Roelofs <p.arroelofs@gmail.com>
2025-11-21 13:02:48 +0100

    chore(cargo): small talk

--- crates/app/eur-timeline/src/manager.rs
+++ crates/app/eur-timeline/src/manager.rs
@@ -312,7 +312,7 @@ impl TimelineManager {
     }
 
     /// Construct messages from current activity snapshots by ids
-    pub async fn construct_snapshot_messages_by_ids(&self, ids: &[String]) -> Vec<Message> {
+    pub async fn construct_snapshot_messages_by_ids(&self, _ids: &[String]) -> Vec<Message> {
         let storage = self.storage.lock().await;
         if let Some(activity) = storage.get_current_activity() {
             if let Some(snapshot) = activity.snapshots.last() {
