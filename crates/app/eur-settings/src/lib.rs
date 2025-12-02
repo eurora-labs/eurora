21f6c4ab371514ea1046cfec6f51b69e41f56b65
Andre Roelofs <andre@eurora-labs.com>
2025-12-02 12:34:24 +0100

    refactor(hotkey): remove the rest of the eur-settings code

--- crates/app/eur-settings/src/lib.rs
+++ crates/app/eur-settings/src/lib.rs
@@ -1,13 +1,11 @@
 use serde::{Deserialize, Serialize};
 use specta::Type;
 
-mod hotkey;
 mod json;
 mod persistence;
 mod settings;
 mod watch;
 
-pub use hotkey::Hotkey;
 pub use settings::{BackendSettings, BackendType, GeneralSettings, TelemetrySettings};
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Type)]
