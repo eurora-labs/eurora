62b95f0cc2eb4a8d135b2b14e41458927b87fb18
Andre Roelofs <andre@eurora-labs.com>
2025-12-02 12:31:32 +0100

    refactor(hotkey): remove the rest of the user hotkey code

--- crates/app/eur-user/src/user.rs
+++ crates/app/eur-user/src/user.rs
@@ -2,24 +2,6 @@ use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Default)]
-pub enum HotkeyFunction {
-    #[default]
-    OpenLauncher,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Default)]
-pub struct Hotkey {
-    pub key: String,
-    pub modifiers: Vec<String>,
-    pub function: HotkeyFunction,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Default)]
-pub struct Hotkeys {
-    pub open_launcher: Hotkey,
-}
-
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct User {
     pub id: Uuid,
@@ -27,6 +9,4 @@ pub struct User {
     pub email: Option<String>,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
-
-    pub hotkeys: Hotkeys,
 }
