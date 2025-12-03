d0ad658a8b12a1035ebf90e6c8b0296b19c1289c
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2025-12-03 14:55:21 +0100

    feat(pdf-watcher): add placeholder assets retrieval

--- apps/extension/chromium/background-script/src/index.ts
+++ apps/extension/chromium/background-script/src/index.ts
@@ -1,9 +1,10 @@
-// import './lib/pdfjs/options/migration.ts';
-// import './lib/pdfjs/preserve-referer.ts';
-// import './lib/pdfjs/pdfHandler.ts';
-// import './lib/pdfjs/extension-router.ts';
-// import './lib/pdfjs/suppress-update.ts';
-// import './lib/pdfjs/telemetry.ts';
+import './lib/pdfjs/options/migration.ts';
+import './lib/pdfjs/preserve-referer.ts';
+import './lib/pdfjs/pdfHandler.ts';
+import './lib/pdfjs/extension-router.ts';
+import './lib/pdfjs/suppress-update.ts';
+import './lib/pdfjs/telemetry.ts';
+
 import './lib/service-worker/native-messaging-worker.ts';
 import './lib/background.ts';
 import './bg.ts';
