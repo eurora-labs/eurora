648a4795e9e89095f5f8c26c980efec7d03b4ec8
Andre Roelofs <andre@eurora-labs.com>
2025-12-27 15:39:53 +0100

    style(eslint): fix all the remaining issues

--- apps/desktop/src/routes/settings/+page.svelte
+++ apps/desktop/src/routes/settings/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import { createTauRPCProxy, type GeneralSettings } from '$lib/bindings/bindings.js';
 	import FirstPartyLogin from '$lib/components/FirstPartyLogin.svelte';
-	import * as Card from '@eurora/ui/components/card/index';
 	import { Input } from '@eurora/ui/components/input/index';
 	import { Label } from '@eurora/ui/components/label/index';
 	import { Switch } from '@eurora/ui/components/switch/index';
 	import { onMount } from 'svelte';
-
 
 	const tauRPC = createTauRPCProxy();
 
