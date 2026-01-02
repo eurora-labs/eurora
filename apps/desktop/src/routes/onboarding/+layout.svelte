f68bcf7bfb52f25a2759c2d788207b2fc23fff2b
Andre Roelofs <andre@eurora-labs.com>
2026-01-02 08:24:45 +0100

    feat(desktop): add metrics choices to onboarding flow

--- apps/desktop/src/routes/onboarding/+layout.svelte
+++ apps/desktop/src/routes/onboarding/+layout.svelte
@@ -2,7 +2,7 @@
 	import { type TimelineAppEvent } from '$lib/bindings/bindings.js';
 	import { TAURPC_SERVICE } from '$lib/bindings/taurpcService.js';
 	import { inject } from '@eurora/shared/context';
-	import * as Sidebar from '@eurora/ui/components/sidebar/index';
+	import EuroraIcon from '@eurora/ui/custom-icons/EuroraLogo.svelte';
 	import { platform } from '@tauri-apps/plugin-os';
 	import { onMount } from 'svelte';
 
@@ -28,11 +28,10 @@
 	});
 </script>
 
-<Sidebar.Provider open={false}>
-	<Sidebar.Inset>
-		<div class="flex flex-col h-screen">
-			<div class="flex-1">{@render children?.()}</div>
-			<div class="flex flex-col w-full"></div>
-		</div>
-	</Sidebar.Inset>
-</Sidebar.Provider>
+<div class="flex flex-row h-screen">
+	<div class="w-1/2">{@render children?.()}</div>
+	<div class="flex flex-col gap-4 w-1/2 justify-center items-center">
+		<EuroraIcon size="128" />
+		<p>Designed in the Netherlands</p>
+	</div>
+</div>
--- apps/desktop/src/routes/onboarding/+page.svelte
+++ apps/desktop/src/routes/onboarding/+page.svelte
@@ -1,14 +1,67 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import FirstPartyLogin from '$lib/components/FirstPartyLogin.svelte';
 	import { Button } from '@eurora/ui/components/button/index';
-	import * as Card from '@eurora/ui/components/card/index';
+	import * as Item from '@eurora/ui/components/item/index';
+	import { Switch } from '@eurora/ui/components/switch/index';
+	import ChevronRight from '@lucide/svelte/icons/chevron-right';
+
+	let errorReporting = $state(true);
+	let usageMetrics = $state(true);
+	let nonAnonymousUsageMetrics = $state(false);
 </script>
 
 <div class="relative flex h-full w-full flex-col">
-	<div class="flex flex-col justify-center items-center h-full w-full gap-8 px-8">
-		<h1 class="text-4xl font-bold drop-shadow-lg">Welcome to Eurora!</h1>
-		<Card.Root
+	<div class="flex flex-col justify-center items-left h-full w-full px-8">
+		<article class="pb-4">
+			<h1 class="text-4xl font-bold drop-shadow-lg pb-4">Welcome to Eurora!</h1>
+			<p class="pb-2">
+				Eurora uses these metrics strictly to help us improve the app. We do not collect any
+				personal information unless you yourself choose to provide it.
+			</p>
+			<p>
+				I ask you to please keep these settings enabled. Eurora is self-funded and these
+				metrics are essential for us to improve the app and stay competitive with
+				billionaire-funded apps.
+			</p>
+		</article>
+
+		<Item.Root variant="default">
+			<Item.Content>
+				<Item.Title>Error reporting</Item.Title>
+				<Item.Description>A simple item with title and description.</Item.Description>
+			</Item.Content>
+			<Item.Actions>
+				<Switch bind:checked={errorReporting} />
+			</Item.Actions>
+		</Item.Root>
+
+		<Item.Root variant="default">
+			<Item.Content>
+				<Item.Title>Usage metrics</Item.Title>
+				<Item.Description>A simple item with title and description.</Item.Description>
+			</Item.Content>
+			<Item.Actions>
+				<Switch bind:checked={usageMetrics} />
+			</Item.Actions>
+		</Item.Root>
+
+		<Item.Root variant="default">
+			<Item.Content>
+				<Item.Title>Non-anonymous usage metrics</Item.Title>
+				<Item.Description>A simple item with title and description.</Item.Description>
+			</Item.Content>
+			<Item.Actions>
+				<Switch bind:checked={nonAnonymousUsageMetrics} />
+			</Item.Actions>
+		</Item.Root>
+		<div class="flex justify-end">
+			<Button variant="default" onclick={() => goto('/onboarding/login')}
+				>Continue
+				<ChevronRight />
+			</Button>
+		</div>
+
+		<!-- <Card.Root
 			class="flex group cursor-pointer w-1/2"
 			onclick={() => goto('/onboarding/first-party')}
 		>
@@ -16,12 +69,11 @@
 				<Card.Title class="mb-2 text-2xl font-semibold">Get started with Eurora</Card.Title>
 				<Card.Description class="">Fastest way to get started.</Card.Description>
 			</Card.Header>
-			<!-- <Card.Content class="flex justify-center">Easiest and fastest way to get started.</Card.Content> -->
-		</Card.Root>
+		</Card.Root> -->
 		<!-- First party login card -->
-		<Button variant="ghost" onclick={() => goto('/onboarding/local')}
+		<!-- <Button variant="ghost" onclick={() => goto('/onboarding/local')}
 			>Or set up a local or a remote LLM provider</Button
-		>
+		> -->
 	</div>
 
 	<!-- Footer space -->
