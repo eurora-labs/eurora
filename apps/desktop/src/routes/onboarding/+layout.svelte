3426a9bf60d6a6ad686ae54cce3ea64cb86a90dc
Andre Roelofs <andre@eurora-labs.com>
2026-01-02 12:45:19 +0100

    fix(coderabbit): comments and improve error handling

--- apps/desktop/src/routes/onboarding/+layout.svelte
+++ apps/desktop/src/routes/onboarding/+layout.svelte
@@ -8,23 +8,29 @@
 
 	let taurpcService = inject(TAURPC_SERVICE);
 	let timelineItems: TimelineAppEvent[] = $state([]);
+	let newAppEventListener: () => void;
 
 	let { children } = $props();
 	onMount(() => {
 		if (document) {
 			document.body.classList.add(`${platform()}-app`);
 		}
-		// taurpcService.timeline.list().then((items) => {
-		// 	timelineItems = items;
-		// });
 
-		taurpcService.timeline.new_app_event.on((e) => {
-			// Limit the items to 5
-			if (timelineItems.length >= 5) {
-				timelineItems.shift();
-			}
-			timelineItems.push(e);
-		});
+		taurpcService.timeline.new_app_event
+			.on((e) => {
+				// Limit the items to 5
+				if (timelineItems.length >= 5) {
+					timelineItems.shift();
+				}
+				timelineItems.push(e);
+			})
+			.then((listener) => {
+				newAppEventListener = listener;
+			});
+
+		return () => {
+			newAppEventListener?.();
+		};
 	});
 </script>
 
