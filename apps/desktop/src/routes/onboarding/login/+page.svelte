43218983e504d74f3498a363b1b3026687f0ac8f
Andre Roelofs <andre@eurora-labs.com>
2026-01-02 09:54:01 +0100

    refactor(desktop): create a hierarchical representation of the routes

--- apps/desktop/src/routes/onboarding/login/+page.svelte
+++ apps/desktop/src/routes/onboarding/login/+page.svelte
@@ -6,7 +6,7 @@
 <div class="flex flex-col justify-center items-center h-full p-8">
 	<Card.Root
 		class="flex group cursor-pointer w-full"
-		onclick={() => goto('/onboarding/first-party')}
+		onclick={() => goto('/onboarding/login/first-party')}
 	>
 		<Card.Header class="pb-6 text-left ">
 			<Card.Title class="mb-2 text-2xl font-semibold">Log In/Sign Up</Card.Title>
