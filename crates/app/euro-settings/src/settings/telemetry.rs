e9d6460c243f1ee685077c8cab137ed33578f6ed
Andre Roelofs <andre@eurora-labs.com>
2026-01-02 09:04:53 +0100

    feat(euro-settings): add telemetrySettings considered field

--- crates/app/euro-settings/src/settings/telemetry.rs
+++ crates/app/euro-settings/src/settings/telemetry.rs
@@ -4,6 +4,7 @@ use specta::Type;
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Type)]
 #[serde(rename_all = "camelCase")]
 pub struct TelemetrySettings {
+    pub considered: bool,
     /// Anonymous metrics
     pub anonymous_metrics: bool,
     /// Anonymous error reporting
@@ -12,4 +13,16 @@ pub struct TelemetrySettings {
     pub non_anonymous_metrics: bool,
     /// Distinct ID, if non-anonymous metrics are enabled
     pub distinct_id: Option<String>,
+}
+
+impl Default for TelemetrySettings {
+    fn default() -> Self {
+        Self {
+            considered: false,
+            anonymous_metrics: true,
+            anonymous_errors: true,
+            non_anonymous_metrics: false,
+            distinct_id: None,
+        }
+    }
 }
