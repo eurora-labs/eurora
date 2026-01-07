8912d93d8da898afa3b85e80327911edf5744fe9
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2026-01-07 17:13:08 +0100

    refactor(euro-assets-service): to euro-asset-service

--- crates/app/euro-auth/src/lib.rs
+++ crates/app/euro-auth/src/lib.rs
@@ -1,12 +1,3 @@
-use crate::proto::{
-    EmailPasswordCredentials, GetLoginTokenResponse, LoginByLoginTokenRequest, LoginRequest,
-    RefreshTokenRequest, RegisterRequest, TokenResponse, login_request::Credential,
-    proto_auth_service_client::ProtoAuthServiceClient,
-};
-use anyhow::{Ok, Result, anyhow};
-use tonic::transport::{Channel, ClientTlsConfig};
-use tracing::{debug, error};
-
 mod client;
 mod interceptor;
 mod manager;
