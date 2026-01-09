62ae26d6dc354bc7b628bd0174d295ca516dc0c3
Andre Roelofs <andre@eurora-labs.com>
2026-01-09 15:33:29 +0100

    refactor(crates/backend): remove server feature from services

--- crates/backend/euro-activity-service/build.rs
+++ crates/backend/euro-activity-service/build.rs
@@ -4,13 +4,11 @@ fn main() -> Result<()> {
     let proto_dir = PathBuf::from("../../../proto");
     let proto_files = vec![PathBuf::from("activity_service.proto")];
 
-    let build_server = std::env::var("CARGO_FEATURE_SERVER").is_ok();
-
     #[cfg(target_os = "windows")]
     {
         let common_dir = PathBuf::from("C:\\protoc\\include");
         tonic_prost_build::configure()
-            .build_server(build_server)
+            .build_server(true)
             .build_client(true)
             .protoc_arg("--experimental_allow_proto3_optional")
             .compile_protos(&proto_files, &[proto_dir, common_dir])?;
@@ -19,7 +17,7 @@ fn main() -> Result<()> {
     #[cfg(not(target_os = "windows"))]
     {
         tonic_prost_build::configure()
-            .build_server(build_server)
+            .build_server(true)
             .build_client(true)
             .protoc_arg("--experimental_allow_proto3_optional")
             .compile_protos(&proto_files, &[proto_dir])?;
