043cf0d4ac9189e7310496aec0420c8f5c15e7b0
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2026-01-19 11:26:22 +0100

    refactor(desktop): remove the base messages for now

--- apps/desktop/src/lib/bindings/bindings.ts
+++ apps/desktop/src/lib/bindings/bindings.ts
@@ -21,6 +21,8 @@ export type ContextChip = { id: string; extension_id: string; name: string; attr
 
 export type Conversation = { id: string | null; title: string; created_at: string; updated_at: string }
 
+export type ConversationView = { id: string | null; title: string }
+
 export type GeneralSettings = { 
 /**
  * Whether to start the app on autostart
@@ -53,20 +55,19 @@ distinctId: string | null }
 
 export type TimelineAppEvent = { name: string; color: string | null; icon_base64: string | null }
 
-const ARGS_MAP = { 'auth':'{"get_login_token":[],"poll_for_login":[]}', 'chat':'{"current_conversation_changed":["conversation"],"send_query":["conversation","channel","query"],"switch_conversation":["conversation_id"]}', 'context_chip':'{"get":[]}', 'monitor':'{"capture_monitor":["monitor_id"]}', 'onboarding':'{"get_browser_extension_download_url":[]}', 'personal_db.conversation':'{"create":[],"get_messages":["conversation_id"],"list":["limit","offset"],"new_conversation_added":["conversation"]}', 'personal_db.message':'{"get":["conversation_id","limit","offset"]}', 'prompt':'{"disconnect":[],"get_service_name":[],"prompt_service_change":["service_name"],"switch_to_ollama":["base_url","model"],"switch_to_remote":["provider","api_key","model"]}', 'settings':'{"get_all_settings":[],"get_general_settings":[],"get_telemetry_settings":[],"set_general_settings":["general_settings"],"set_telemetry_settings":["telemetry_settings"]}', 'system':'{"check_grpc_server_connection":["server_address"],"list_activities":[]}', 'third_party':'{"check_api_key_exists":[],"save_api_key":["api_key"]}', 'timeline':'{"list":[],"new_app_event":["event"],"new_assets_event":["chips"]}' }
+const ARGS_MAP = { 'auth':'{"get_login_token":[],"poll_for_login":[]}', 'chat':'{"current_conversation_changed":["conversation"],"send_query":["conversation","channel","query"],"switch_conversation":["conversation_id"]}', 'context_chip':'{"get":[]}', 'conversation':'{"create":[],"get_messages":["conversation_id"],"list":["limit","offset"],"new_conversation_added":["conversation"]}', 'monitor':'{"capture_monitor":["monitor_id"]}', 'onboarding':'{"get_browser_extension_download_url":[]}', 'prompt':'{"disconnect":[],"get_service_name":[],"prompt_service_change":["service_name"],"switch_to_ollama":["base_url","model"],"switch_to_remote":["provider","api_key","model"]}', 'settings':'{"get_all_settings":[],"get_general_settings":[],"get_telemetry_settings":[],"set_general_settings":["general_settings"],"set_telemetry_settings":["telemetry_settings"]}', 'system':'{"check_grpc_server_connection":["server_address"],"list_activities":[]}', 'third_party':'{"check_api_key_exists":[],"save_api_key":["api_key"]}', 'timeline':'{"list":[],"new_app_event":["event"],"new_assets_event":["chips"]}' }
 export type Router = { "auth": {get_login_token: () => Promise<LoginToken>, 
 poll_for_login: () => Promise<boolean>},
 "chat": {current_conversation_changed: (conversation: Conversation) => Promise<void>, 
 send_query: (conversation: Conversation, channel: TAURI_CHANNEL<ResponseChunk>, query: Query) => Promise<string>, 
 switch_conversation: (conversationId: string) => Promise<Conversation>},
 "context_chip": {get: () => Promise<ContextChip[]>},
+"conversation": {create: () => Promise<Conversation>, 
+get_messages: (conversationId: string) => Promise<string[]>, 
+list: (limit: number, offset: number) => Promise<ConversationView[]>, 
+new_conversation_added: (conversation: Conversation) => Promise<void>},
 "monitor": {capture_monitor: (monitorId: string) => Promise<string>},
 "onboarding": {get_browser_extension_download_url: () => Promise<string>},
-"personal_db.conversation": {create: () => Promise<Conversation>, 
-get_messages: (conversationId: string) => Promise<string[]>, 
-list: (limit: number, offset: number) => Promise<Conversation[]>, 
-new_conversation_added: (conversation: Conversation) => Promise<void>},
-"personal_db.message": {get: (conversationId: string, limit: number | null, offset: number | null) => Promise<string[]>},
 "prompt": {disconnect: () => Promise<null>, 
 get_service_name: () => Promise<string>, 
 prompt_service_change: (serviceName: string | null) => Promise<void>, 
