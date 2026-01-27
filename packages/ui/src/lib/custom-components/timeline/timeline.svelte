8a92db7be8526ca284cd8d3f426b856be996f197
Andre Roelofs <andre@eurora-labs.com>
2026-01-27 11:34:55 +0100

    fix(desktop): incorrect outdated styling

--- packages/ui/src/lib/custom-components/timeline/timeline.svelte
+++ packages/ui/src/lib/custom-components/timeline/timeline.svelte
@@ -20,10 +20,6 @@
 </script>
 
 <script lang="ts">
-	import * as Collapsible from '$lib/components/collapsible/index.js';
-	import ChevronDown from '@lucide/svelte/icons/chevron-down';
-	import ChevronUp from '@lucide/svelte/icons/chevron-up';
-
 	let {
 		class: className,
 		children,
@@ -38,46 +34,19 @@
 			open = defaultOpen;
 		}
 	});
+
+	export { TimelineProps };
 </script>
 
-<Collapsible.Root bind:open class="w-full {className}">
-	<Collapsible.Trigger class="w-full">
-		<div class="flex flex-row w-full justify-end items-end">
-			{#if open}
-				<ChevronDown class="h-4 w-4" />
-			{:else}
-				<ChevronUp class="h-4 w-4" />
-			{/if}
-		</div>
-	</Collapsible.Trigger>
-	{#if !open}
-		<div class="flex flex-col">
-			<div class="flex flex-row mb-4 h-8">
-				<div class="flex w-1/2 items-center justify-end">
-					<div class="flex flex-row w-max gap-2">
-						{@render children?.()}
-					</div>
-				</div>
-				<div class="flex w-fit justify-center pl-2 ml-4 border-l-2 items-center">
-					{label}
-				</div>
+<div class="flex flex-col {className}">
+	<div class="flex flex-row mb-4 h-8">
+		<div class="flex w-1/2 items-center justify-end">
+			<div class="flex flex-row w-max gap-2">
+				{@render children?.()}
 			</div>
 		</div>
-	{/if}
-
-	<Collapsible.Content>
-		<div class="flex flex-col">
-			<div class="flex w-full justify-center mb-4">{label}</div>
-			<div class="flex flex-row mb-4 h-[100px]">
-				<div class="flex w-1/2 items-center justify-end">
-					<div class="flex flex-row w-max gap-2">
-						{@render children?.()}
-					</div>
-				</div>
-				<div class="flex w-[200px] gap-2 h-full">
-					<div class="h-full w-[5px] border rounded-full"></div>
-				</div>
-			</div>
+		<div class="flex w-fit justify-center pl-2 ml-4 border-l-2 items-center">
+			{label}
 		</div>
-	</Collapsible.Content>
-</Collapsible.Root>
+	</div>
+</div>
