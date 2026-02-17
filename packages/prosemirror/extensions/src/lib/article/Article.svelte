4a6b9bb056fc25f89270a97c8570be2510e54cb2
Andre Roelofs <andre@eurora-labs.com>
2026-02-17 13:46:17 +0100

    fix(prosemirror): artifacts of past versions still showing popover blocks

--- packages/prosemirror/extensions/src/lib/article/Article.svelte
+++ packages/prosemirror/extensions/src/lib/article/Article.svelte
@@ -65,9 +65,6 @@
 </script>
 
 <script lang="ts">
-	import { Input } from '@eurora/ui/components/input/index';
-	import { Label } from '@eurora/ui/components/label/index';
-	import * as Popover from '@eurora/ui/components/popover/index';
 	import { ContextChip } from '@eurora/ui/custom-components/context-chip/index';
 	import type { SvelteNodeViewProps } from '@eurora/prosemirror-core/index';
 	export interface Props extends SvelteNodeViewProps<ArticleAttrs> {
@@ -88,39 +85,7 @@
 	}
 </script>
 
-<Popover.Root>
-	<Popover.Trigger>
-		<ContextChip bind:ref data-hole {...attrs} onkeydown={handleKeyDown}
-			>{attrs.name}</ContextChip
-		>
-	</Popover.Trigger>
-	<Popover.Content class="w-80">
-		<div class="grid gap-4">
-			<div class="space-y-2">
-				<h4 class="font-medium leading-none">Dimensions</h4>
-				<p class="text-muted-foreground text-sm">Set the dimensions for the layer.</p>
-			</div>
-			<div class="grid gap-2">
-				<div class="grid grid-cols-3 items-center gap-4">
-					<Label for="width">Width</Label>
-					<Input id="width" value="100%" class="col-span-2 h-8" />
-				</div>
-				<div class="grid grid-cols-3 items-center gap-4">
-					<Label for="maxWidth">Max. width</Label>
-					<Input id="maxWidth" value="300px" class="col-span-2 h-8" />
-				</div>
-				<div class="grid grid-cols-3 items-center gap-4">
-					<Label for="height">Height</Label>
-					<Input id="height" value="25px" class="col-span-2 h-8" />
-				</div>
-				<div class="grid grid-cols-3 items-center gap-4">
-					<Label for="maxHeight">Max. height</Label>
-					<Input id="maxHeight" value="none" class="col-span-2 h-8" />
-				</div>
-			</div>
-		</div>
-	</Popover.Content>
-</Popover.Root>
-
-<style lang="postcss">
-</style>
+<!-- <ContextChip bind:ref data-hole {...attrs} onkeydown={handleKeyDown}>{attrs.name}</ContextChip> -->
+<ContextChip bind:ref data-hole {...attrs} onkeydown={handleKeyDown}
+	>www.hydration-water.com</ContextChip
+>
