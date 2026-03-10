a912211c79fde1115fc180a84ab9b9c966401766
Andre Roelofs <andre@eurora-labs.com>
2026-03-10 17:14:38 +0100

    feat(euro-native-messaging): add more twitter types

--- apps/browser/src/content/sites/x.com/parser/types.ts
+++ apps/browser/src/content/sites/x.com/parser/types.ts
@@ -1,36 +1,9 @@
-import type { NativeTwitterTweet } from '../../../../shared/content/bindings';
-
-export interface TweetPageData {
-	tweet: NativeTwitterTweet | null;
-	replies: NativeTwitterTweet[];
-}
-
-export interface ProfilePageData {
-	username: string;
-	tweets: NativeTwitterTweet[];
-}
-
-export interface TimelineData {
-	tweets: NativeTwitterTweet[];
-}
-
-export interface SearchData {
-	query: string;
-	tweets: NativeTwitterTweet[];
-}
-
-export interface NotificationsData {
-	tweets: NativeTwitterTweet[];
-}
-
-export interface UnsupportedPageData {
-	url: string;
-}
-
-export type ParseResult =
-	| { page: 'tweet'; data: TweetPageData }
-	| { page: 'profile'; data: ProfilePageData }
-	| { page: 'home'; data: TimelineData }
-	| { page: 'search'; data: SearchData }
-	| { page: 'notifications'; data: NotificationsData }
-	| { page: 'unsupported'; data: UnsupportedPageData };
+export type {
+	TweetPageData,
+	ProfilePageData,
+	TimelineData,
+	SearchData,
+	NotificationsData,
+	UnsupportedPageData,
+	ParseResult,
+} from '../../../../shared/content/bindings';
