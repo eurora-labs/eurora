b6104434ffba029d0d26fe80b4e233fd38e44cd6
Andre Roelofs <andre@eurora-labs.com>
2026-03-12 10:44:33 +0100

    feat(web): add nav bar to every page

--- apps/web/src/lib/components/MenuBar.svelte
+++ apps/web/src/lib/components/MenuBar.svelte
@@ -1,26 +1,52 @@
 <script lang="ts">
+	import UserButton from '$lib/components/UserButton.svelte';
+	import { isAuthenticated } from '$lib/stores/auth.js';
 	import { Button } from '@eurora/ui/components/button/index';
+	import * as NavigationMenu from '@eurora/ui/components/navigation-menu/index';
+	import { navigationMenuTriggerStyle } from '@eurora/ui/components/navigation-menu/navigation-menu-trigger.svelte';
 	import EuroraLogo from '@eurora/ui/custom-icons/EuroraLogo.svelte';
-	import type { Snippet } from 'svelte';
-
-	let {
-		actions,
-	}: {
-		actions?: Snippet;
-	} = $props();
+	import SiGithub from '@icons-pack/svelte-simple-icons/icons/SiGithub';
+	import LogInIcon from '@lucide/svelte/icons/log-in';
 </script>
 
 <div class="bg-transparent z-0 flex items-center justify-between px-6 py-4 mt-2">
-	<div class="flex items-center gap-2">
-		<Button variant="link" href="/" class="decoration-transparent">
-			<EuroraLogo style="width: 2rem; height: 2rem;" />
-			<span class="text-lg text-primary-foreground font-bold">Eurora</span>
-		</Button>
-	</div>
+	<Button variant="link" href="/" class="decoration-transparent">
+		<EuroraLogo style="width: 2rem; height: 2rem;" />
+		<span class="text-lg text-primary-foreground font-bold">Eurora</span>
+	</Button>
 
-	{#if actions}
-		<div class="flex items-center gap-4">
-			{@render actions()}
-		</div>
-	{/if}
+	<div class="flex items-center gap-4">
+		<Button variant="default" href="/download">Download</Button>
+
+		<NavigationMenu.Root>
+			<NavigationMenu.List>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="/docs" class={navigationMenuTriggerStyle()}>Docs</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="/pricing" class={navigationMenuTriggerStyle()}>Pricing</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+			</NavigationMenu.List>
+		</NavigationMenu.Root>
+
+		<Button variant="ghost" size="icon" href="https://github.com/eurora-labs/eurora">
+			<SiGithub />
+		</Button>
+		{#if $isAuthenticated}
+			<UserButton />
+		{:else}
+			<Button variant="outline" href="/login" class="backdrop-blur-2xl">
+				Login
+				<LogInIcon />
+			</Button>
+		{/if}
+	</div>
 </div>
