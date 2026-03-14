7f2f66d06ae1a003feb4c7b94a46899edd7f2635
Andre Roelofs <andre@eurora-labs.com>
2026-03-14 16:56:12 +0100

    feat(desktop): improve onboarding

--- crates/app/euro-tauri/src/main.rs
+++ crates/app/euro-tauri/src/main.rs
@@ -298,14 +298,13 @@ fn setup_main_window(
     }
 
     let handle = tauri_app.handle().clone();
-    main_window.on_window_event(move |event| match event {
-        tauri::WindowEvent::CloseRequested { api, .. } => {
+    main_window.on_window_event(move |event| {
+        if let tauri::WindowEvent::CloseRequested { api, .. } = event {
             if let Some(w) = handle.get_webview_window("main") {
                 let _ = w.minimize();
             }
             api.prevent_close();
         }
-        _ => {}
     });
 
     Ok(())
