ba25e703785e51fcfc092af2653ff0133f60d7b5
Andre Roelofs <andre@eurora-labs.com>
2026-03-14 14:39:37 +0100

    refactor(desktop): the user service is now a reactive mode of communition with tauri

--- apps/desktop/src/lib/components/FirstPartyLogin.svelte
+++ apps/desktop/src/lib/components/FirstPartyLogin.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { TAURPC_SERVICE } from '$lib/bindings/taurpcService.js';
+	import { USER_SERVICE } from '$lib/services/user-service.svelte.js';
 	import { inject } from '@eurora/shared/context';
 	import * as Card from '@eurora/ui/components/card/index';
 	import { open } from '@tauri-apps/plugin-shell';
 
-	const taurpc = inject(TAURPC_SERVICE);
+	const user = inject(USER_SERVICE);
 	async function openLogin() {
-		const loginToken = await taurpc.auth.get_login_token();
+		const loginToken = await user.getLoginToken();
 		await open(loginToken.url);
 
 		const interval = setInterval(async () => {
-			const isLoginSuccess = await taurpc.auth.poll_for_login();
+			const isLoginSuccess = await user.pollForLogin();
 			if (!isLoginSuccess) {
 				return;
 			}
