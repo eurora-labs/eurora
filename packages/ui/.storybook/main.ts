6a62cf2a25ce0889f837a4043c5271d06e28e2f0
Andre Roelofs <andre@eurora-labs.com>
2026-03-15 12:08:43 +0100

    feat(ui): add stories for chain of thought and messages and upgrade storybook to v10

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -2,19 +2,10 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.stories.@(js|ts|svelte)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-docs',
-		'@storybook/addon-svelte-csf',
-		'storybook-dark-mode',
-	],
+	addons: ['@storybook/addon-links', '@storybook/addon-docs', '@storybook/addon-svelte-csf'],
 	framework: {
 		name: '@storybook/sveltekit',
 		options: {},
-	},
-	typescript: {
-		check: false,
 	},
 	staticDirs: ['../static'],
 };
