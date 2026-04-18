48f9cf17e9dbbc803613a5b646521703bd1848d3
Andre Roelofs <33526246+AndreRoelofs@users.noreply.github.com>
2026-04-18 10:04:34 +0200

    fix(desktop): losing focus on sending message

--- apps/mobile/src/routes/(app)/[[id]]/+page.svelte
+++ apps/mobile/src/routes/(app)/[[id]]/+page.svelte
@@ -23,7 +23,7 @@
 		const newThread = chatService.newThread;
 		if (newThread) {
 			chatService.newThread = undefined;
-			goto(`/${newThread.id}`, { replaceState: true });
+			goto(`/${newThread.id}`, { replaceState: true, keepFocus: true });
 		}
 	});
 
